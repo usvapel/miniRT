acbbf4c13ccb09f18c8681234a2af3c2071041ec
Julius Pellinen <jpelline@c2r5p8.hive.fi>
2025-09-15 21:30:04 +0300

    started doing parsing

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: jpelline <jpelline@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 13:05:20 by jpelline          #+#    #+#             */
-/*   Updated: 2025/09/02 13:05:53 by jpelline         ###   ########.fr       */
+/*   Updated: 2025/09/15 15:00:47 by jpelline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,58 @@
 # define WINDOW_WIDTH 800
 # define WINDOW_HEIGHT 800
 
+typedef struct s_camera t_camera;
+typedef struct s_color t_color;
+typedef struct s_vec3d t_vec3d;
+typedef enum e_id t_id;
+
+typedef struct s_engine
+{
+	int32_t	monitor_width;
+	int32_t monitor_height;
+	mlx_t *mlx;
+	mlx_image_t *image_background;
+	mlx_image_t *objects;
+	t_camera *camera;
+}	t_engine;
+
+typedef struct s_camera
+{
+	t_vec3d *pos;
+	t_vec3d *dir;
+	int fov;
+} t_camera;
+
+typedef struct s_sphere
+{
+	int type;
+	t_vec3d *pos;
+	float diameter;
+	t_color *color;
+} t_sphere;
+
+typedef struct s_vec3d
+{
+	float x;
+	float y;
+	float z;
+} t_vec3d;
+
+typedef struct s_color
+{
+	int r;
+	int g;
+	int b;
+	int a;
+} t_color;
+
+typedef enum e_id
+{
+	AMBIENT_LIGHNING,
+	CAMERA,
+	LIGHT,
+	SPHERE,
+	PLANE,
+	CYLINDER
+} t_id ;
 #endif // MINIRT_T
