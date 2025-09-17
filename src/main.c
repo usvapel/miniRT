7f40107e5fa871c2f16d764e5598427923756a18
Julius Pellinen <jpelline@c2r6p3.hive.fi>
2025-09-17 16:15:50 +0300

    working in progress

--- src/main.c
+++ src/main.c
@@ -10,12 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "minirt.h"
-
-// void init_A(t_engine *engine, char **split)
-// {
-//
-// }
+#include <stdlib.h>
 
 void free_array(void **array)
 {
@@ -28,6 +25,18 @@ void free_array(void **array)
 	free(array);
 }
 
+void exit_on_parsing_failure(char *s, char **ss, char **sss[3])
+{
+	free(s);
+	free_array( (void *) ss);
+	if (!sss)
+		exit(EXIT_FAILURE);
+	free_array( (void *) sss[0] );
+	free_array( (void *) sss[1] );
+	free_array( (void *) sss[2] );
+	exit(EXIT_FAILURE);
+}
+
 void print_array(char **arr)
 {
 	while (*arr)
@@ -37,26 +46,28 @@ void print_array(char **arr)
 void init_C(t_engine *engine, char **split)
 {
 	char **pos[3];
-	puts("here");
-	pos[0] = ft_split(split[2], ',');
-	pos[1] = ft_split(split[3], ',');
-	pos[2] = ft_split(split[4], ',');
-	print_array(pos[0]);
-	print_array(pos[1]);
-	print_array(pos[2]);
-	if (!pos[0])
-		exit(1);
-	engine->camera->pos->x = ft_atof(pos[0][0]);
-	engine->camera->pos->y = ft_atof(pos[0][1]);
-	engine->camera->pos->z = ft_atof(pos[0][2]);
-	free_array((void *)pos[0]);
-	free_array((void *)pos[1]);
-	free_array((void *)pos[2]);
-	// engine->camera->dir->x;
-	// engine->camera->dir->y;
-	// engine->camera->dir->z;
-	//
-	// engine->camera->fov;
+	pos[0] = ft_split(split[1], ',');
+	pos[1] = ft_split(split[2], ',');
+	pos[2] = ft_split(split[3], ',');
+	if (!pos[0] || !pos[1] || !pos[2])
+		exit_on_parsing_failure(NULL, NULL, pos);
+	engine->camera.pos.x = ft_atof(pos[0][0]);
+	engine->camera.pos.y = ft_atof(pos[0][1]);
+	engine->camera.pos.z = ft_atof(pos[0][2]);
+	engine->camera.dir.x = ft_atof(pos[1][0]);
+	engine->camera.dir.y = ft_atof(pos[1][1]);
+	engine->camera.dir.z = ft_atof(pos[1][2]);
+	engine->camera.fov = ft_atof(pos[2][0]);
+	free_array( (void *) pos[0] );
+	free_array( (void *) pos[1] );
+	free_array( (void *) pos[2] );
+}
+
+void init_sp(t_engine *engine, char **split)
+{
+	(void)engine;
+	(void)split;
+	return ;
 }
 
 void set_values(t_engine *engine, char **split)
@@ -69,24 +80,38 @@ void set_values(t_engine *engine, char **split)
 		init_C(engine, split);
 	// init_L();
 	// if (!ft_strcmp(split[1], "sp"))
-	// 	init_sp();
+	if (ft_strcmp(split[1], "sp"))
+		init_sp(engine, split);
 	// init_pl();
 	// init_cy();
-//test
 }
 
 void input_parsing(t_engine *engine, char **av)
 {
 	char *line;
 	char **split;
-	int fd = open(av[1], O_RDONLY);
+	int fd;
+
+	fd = open(av[1], O_RDONLY);
 	if (fd < 0)
 		exit(1);
-	line = get_next_line(fd);
-	if (!line)
-		return ;
-	split = ft_split(line, ' ');
-	set_values(engine, split);
+	while (true)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		split = ft_split(line, ' ');
+		if (!split)
+		{
+			close(fd);
+			free(line);
+			free_array((void *)split);
+			exit(EXIT_FAILURE);
+		}
+		free(line);
+		set_values(engine, split);
+		free_array((void *)split);
+	}
 	close(fd);
 }
 
@@ -100,9 +125,11 @@ void key_hook(mlx_key_data_t keydata, void *param)
 
 int main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	if (ac < 2)
+		return (0);
 	t_engine *engine = ft_calloc(1, sizeof(t_engine));
+	if (!engine)
+		return (1);
 	input_parsing(engine, av);
 	mlx_set_setting(MLX_HEADLESS, true);
 	engine->mlx = mlx_init(10, 10, "miniRT", true);
