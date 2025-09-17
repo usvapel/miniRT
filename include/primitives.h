e12f19742af04f7c60e4bf1db81da6cdfa8161c4
Zakaria Farah <zakaria.farah55@outlook.com>
2025-09-17 15:03:17 +0300

    header for primitives

---
+++ include/primitives.h
@@ -0,0 +1,19 @@
+#ifndef PRIMITIVES_H
+#define PRIMITIVES_H
+
+typedef struct s_vec3d
+{
+	float x;
+	float y;
+	float z;
+} t_vec3d;
+
+typedef struct s_color
+{
+	int r;
+	int g;
+	int b;
+	int a;
+} t_color;
+
+#endif
