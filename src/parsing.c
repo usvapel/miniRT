37384606042446f75a902be49b65f71ebce5bf8e
julius <julius.pellinen@gmail.com>
2025-09-19 13:10:58 +0300

    updates to parsing, still needs some work

--- src/parsing.c
+++ src/parsing.c
@@ -1,15 +1,15 @@
 
 #include "minirt.h"
 
-void runtime_error(char *s)
+void	runtime_error(char *s)
 {
 	printf("Error: %s\n", s);
 	exit(EXIT_FAILURE);
 }
 
-void free_array(void **array)
+void	free_array(void **array)
 {
-	void **copy;
+	void	**copy;
 
 	if (!array)
 		return ;
@@ -19,15 +19,15 @@ void free_array(void **array)
 	free(array);
 }
 
-void print_array(char **arr)
+void	print_array(char **arr)
 {
 	while (*arr)
 		ft_printf("%s ", *arr++);
 }
 
-static t_vec3d parse_vec3d(char **components)
+static t_vec3d	parse_vec3d(char **components)
 {
-	t_vec3d vec;
+	t_vec3d	vec;
 
 	vec.x = ft_atof(components[0]);
 	vec.y = ft_atof(components[1]);
@@ -35,7 +35,18 @@ static t_vec3d parse_vec3d(char **components)
 	return (vec);
 }
 
-void validate_camera(t_engine *engine)
+static t_color	parse_color(char **components)
+{
+	t_color	color;
+
+	color.r = ft_atof(components[0]);
+	color.g = ft_atof(components[1]);
+	color.b = ft_atof(components[2]);
+	color.a = 255;
+	return (color);
+}
+
+void	validate_camera(t_engine *engine)
 {
 	if (engine->camera.fov < 0 || engine->camera.fov > 180)
 		runtime_error("Invalid FOV value");
@@ -47,16 +58,18 @@ void validate_camera(t_engine *engine)
 		runtime_error("Invalid orientation z value");
 }
 
-void free_values(char **values[3])
+void	free_values(char **values[3])
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	while (i < 3)
-		free_array( (void *) values[i++] );
+		free_array((void *)values[i++]);
 }
 
-char **safe_split(char **values[3], char *line)
+char	**safe_split(char **values[3], char *line)
 {
-	char **splitted;
+	char	**splitted;
 
 	splitted = ft_split(line, ',');
 	if (!splitted)
@@ -67,11 +80,12 @@ char **safe_split(char **values[3], char *line)
 	return (splitted);
 }
 
-void init_camera(t_engine *engine, char **split)
+void	init_camera(t_engine *engine, char **split)
 {
-	char **values[3];
+	char	**values[3];
 
 	*values = NULL;
+	puts("initializing camera");
 	values[0] = safe_split(values, split[1]);
 	values[1] = safe_split(values, split[2]);
 	values[2] = safe_split(values, split[3]);
@@ -82,57 +96,38 @@ void init_camera(t_engine *engine, char **split)
 	validate_camera(engine);
 }
 
-int init_sp(t_engine *engine, char **split)
+void	init_sp(t_engine *engine, char **split)
 {
-	char **values[3];
+	char	**values[3];
 
-	values[0] = ft_split(split[1], ',');
-	values[1] = ft_split(split[2], ',');
-	values[2] = ft_split(split[3], ',');
-	if (!values[0] || !values[1] || !values[2])
-	{
-		free_array( (void *) values[0] );
-		free_array( (void *) values[1] );
-		free_array( (void *) values[2] );
-		exit(EXIT_FAILURE);
-	}
+	*values = NULL;
+	puts("initializing sphere");
+	values[0] = safe_split(values, split[1]);
+	values[1] = safe_split(values, split[2]);
+	values[2] = safe_split(values, split[3]);
 	engine->sphere.type = SPHERE;
-	engine->sphere.pos.x = ft_atof(values[0][1]);
-	engine->sphere.pos.y = ft_atof(values[0][2]);
-	engine->sphere.pos.z = ft_atof(values[0][3]);
+	engine->sphere.pos = parse_vec3d(values[0]);
 	engine->sphere.diameter = ft_atof(values[1][0]);
-	engine->sphere.color.r = ft_atof(values[2][0]);
-	engine->sphere.color.g = ft_atof(values[2][1]);
-	engine->sphere.color.b = ft_atof(values[2][2]);
-	engine->sphere.color.a = 255;
-	free_array( (void *) values[0] );
-	free_array( (void *) values[1] );
-	free_array( (void *) values[2] );
-	return (0);
+	engine->sphere.color = parse_color(values[2]);
+	free_values(values);
 }
 
-void set_values(t_engine *engine, char **split)
+void	set_values(t_engine *engine, char **split)
 {
 	if (!split[0])
 		return ;
-	// if (!ft_strcmp(split[1], "A"))
-	// 	init_A(engine, split);
 	if (ft_strcmp(split[0], "C") == 0)
 		return (init_camera(engine, split));
-	// init_L();
-	// if (!ft_strcmp(split[1], "sp"))
-	// if (ft_strcmp(split[0], "sp") == 0)
-	// 	init_sp(engine, split);
-	// init_pl();
-	// init_cy();
+	if (ft_strcmp(split[0], "sp") == 0)
+		return (init_sp(engine, split));
 	printf("invalid identifier: %s\n", split[0]);
 }
 
-void input_parsing(t_engine *engine, char **av)
+void	input_parsing(t_engine *engine, char **av)
 {
-	char *line;
-	char **split;
-	int fd;
+	char	*line;
+	char	**split;
+	int		fd;
 
 	fd = open(av[1], O_RDONLY);
 	if (fd < 0)
