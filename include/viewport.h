b17a547c7248a2ea79254f2b1fa38578c86d114b
jackie jan <zakaria.farah55@outlook.com>
2025-09-19 17:31:43 +0300

    viewport header

---
+++ include/viewport.h
@@ -0,0 +1,20 @@
+#ifndef VIEWPORT_H
+#define VIEWPORT_H
+
+#include "primitives.h"
+
+typedef struct s_viewport
+{
+    float w;
+    float h;
+    float aspect_ratio;
+    t_vec3d hori_axis;
+    t_vec3d vert_axis;
+    t_vec3d start_pixel;
+    t_vec3d step_x;
+    t_vec3d step_y;
+} t_viewport;
+
+void    setup_viewport(void);
+
+#endif
