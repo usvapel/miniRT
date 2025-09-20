4956c293fb9b32eee5a9b4ae9ee7a2e59d8365e1
jackie jan <zakaria.farah55@outlook.com>
2025-09-20 20:06:00 +0300

    added util funcs for vec3d

---
+++ src/vec3d.c
@@ -0,0 +1,40 @@
+#include "primitives.h"
+#include <stdio.h>
+
+t_vec3d new_vec3d(float x, float y, float z)
+{
+    t_vec3d vec3d;
+
+    vec3d.x = x;
+    vec3d.y = y;
+    vec3d.z = z;
+    return vec3d;
+}
+
+void scale_vec3d(t_vec3d *vec, float scaler)
+{
+    vec->x *= scaler;
+    vec->y *= scaler;
+    vec->z *= scaler;
+}
+
+void    add_vec3d(t_vec3d *vec1, t_vec3d vec2)
+{
+    vec1->x += vec2.x;
+    vec1->y += vec2.y;
+    vec1->z += vec2.z;
+}
+
+void    minus_vec3d(t_vec3d *vec1, t_vec3d vec2)
+{
+    vec1->x -= vec2.x;
+    vec1->y -= vec2.y;
+    vec1->z -= vec2.z;
+}
+
+void    print_vec(t_vec3d vec, char *id)
+{
+    if (id)
+        printf("%s ", id);
+	printf("(%f, %f, %f)\n", vec.x, vec.y, vec.z);
+}
