7c6d176fe7aaa1436d7af872df2fb7e53e40f577
Julius Pellinen <julius.pellinen@gmail.com>
2025-09-24 18:03:09 +0300

    updated fps to not use malloc

--- src/fps.c
+++ src/fps.c
@@ -13,23 +13,48 @@ int	get_seconds(t_engine *engine)
 	return ((int)ms);
 }
 
+
+void add_fps(char *title, int fps)
+{
+	int i = 0;
+	int j = 0;
+	int start = 14;
+	int temp = fps;
+	char buf[6];
+
+	if (fps == 0)
+	{
+		title[start] = '0';
+		title[start + 1] = '\0';
+		return ;
+	}
+	while (temp > 0 && i < 5)
+	{
+		buf[i++] = (temp % 10) + '0';
+		temp /= 10;
+	}
+	while (j < i)
+	{
+		title[start + j] = buf[i - j - 1];
+		j++;
+	}
+	title[start + i] = '\0';
+}
+
 void fps_counter(void *param)
 {
 	t_engine *engine;
 	static int fps;
 	static int prev_sec;
 	int curr_sec;
+	char title[20] = "MiniRT | fps: ";
 
 	engine = param;
 	curr_sec = get_seconds(engine);
 	if (curr_sec > prev_sec)
 	{
-		printf("fps: %d\n", fps);
-		char *title = ft_strjoin("MiniRT | fps: ", ft_itoa(fps));
-		if (!title)
-			exit(1);
+		add_fps(title, fps);
 		mlx_set_window_title(engine->mlx, title);
-		free(title);
 		fps = 0;
 	}
 	fps++;
