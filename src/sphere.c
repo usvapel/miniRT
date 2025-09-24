af82c9b610bb3be3c6eb0e4c3589aac9dcf323b2
jackie jan <zakaria.farah55@outlook.com>
2025-09-24 17:17:01 +0300

    added ray-sphere intersection func that set the hit position

--- src/sphere.c
+++ src/sphere.c
@@ -1,5 +1,7 @@
 #include "minirt.h"
 
+float solve_for_hit(t_ray ray, t_sphere sphere, float *t0, float *t1);
+
 bool sphere_ray_hit(t_ray ray, t_sphere sphere)
 {
     const float a = dot_vec3d(ray.udir, ray.udir);
@@ -19,7 +21,6 @@ bool sphere_ray_hit(t_ray ray, t_sphere sphere)
     return ((b * b) - (4 * a * c) >= 0);
 }
 
-
 float sphere_ray_hit_test(t_ray ray, t_sphere sphere)
 {
     const float a = dot_vec3d(ray.udir, ray.udir);
@@ -36,5 +37,58 @@ float sphere_ray_hit_test(t_ray ray, t_sphere sphere)
     scale_vec3d(&tmp, -2);
     b = dot_vec3d(tmp, ray_s_origin);
     c = dot_vec3d(ray_s_origin, ray_s_origin) - (sphere.r * sphere.r);
+    return ((b * b) - (4 * a * c));
+}
+
+
+bool sphere_hit(t_sphere sphere, t_ray ray, t_vec3d *pos)
+{
+    float t0;
+    float t1;
+    float disc;
+    t_vec3d hit;
+    t_vec3d hit2;
+    t_vec3d tmp;
+
+    disc = solve_for_hit(ray, sphere, &t0, &t1);
+    if (disc < 0)
+        return false;
+    hit = ray.origin;  
+    tmp = ray.udir;
+    scale_vec3d(&tmp, t0);
+    add_vec3d(&hit, tmp);
+    hit2 = ray.origin;  
+    tmp = ray.udir;
+    scale_vec3d(&tmp, t1);
+    add_vec3d(&hit2, tmp);
+    if (disc == 0 || t0 < t1)
+        add_vec3d(pos, hit);
+    else
+        add_vec3d(pos, hit2);
+    return true;
+}
+
+float solve_for_hit(t_ray ray, t_sphere sphere, float *t0, float *t1)
+{
+    const float a = dot_vec3d(ray.udir, ray.udir);
+    t_vec3d ray_s_origin;
+    t_vec3d tmp;
+    float b;
+    float c;
+    float disc;
+
+
+    ray_s_origin = sphere.pos;
+    minus_vec3d(&ray_s_origin, ray.origin);
+    tmp = ray.udir;
+    scale_vec3d(&tmp, -2);
+    b = dot_vec3d(tmp, ray_s_origin);
+    c = dot_vec3d(ray_s_origin, ray_s_origin) - (sphere.r * sphere.r);
+    disc = (b * b) - (4 * a * c);
+    if (disc >= 0)
+    {
+        *t0 = (-b - sqrt(disc)) / (2 * a);
+        *t1 = (-b + sqrt(disc)) / (2 * a);
+    }
     return ((b * b) - (4 * a * c));
 }
