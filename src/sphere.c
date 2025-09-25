c3547416d560a910771f5041f4a4e82035c52720
jackie jan <zakaria.farah55@outlook.com>
2025-09-25 22:34:59 +0300

    Refactors and minor fixes

--- src/sphere.c
+++ src/sphere.c
@@ -3,72 +3,23 @@
 
 float solve_for_hit(t_ray ray, t_sphere sphere, float *t0, float *t1);
 
-bool sphere_ray_hit(t_ray ray, t_sphere sphere)
-{
-    const float a = dot_vec3d(ray.udir, ray.udir);
-    t_vec3d ray_s_origin;
-    t_vec3d tmp;
-    float b;
-    float c;
-
-    // print_vec(ray.origin, "Ray ori: ");
-    // print_vec(ray.udir, "Ray dir: ");
-    ray_s_origin = sphere.pos;
-    minus_vec3d(&ray_s_origin, ray.origin);
-    tmp = ray.udir;
-    scale_vec3d(&tmp, -2);
-    b = dot_vec3d(tmp, ray_s_origin);
-    c = dot_vec3d(ray_s_origin, ray_s_origin) - (sphere.r * sphere.r);
-    return ((b * b) - (4 * a * c) >= 0);
-}
-
-float sphere_ray_hit_test(t_ray ray, t_sphere sphere)
-{
-    const float a = dot_vec3d(ray.udir, ray.udir);
-    t_vec3d ray_s_origin;
-    t_vec3d tmp;
-    float b;
-    float c;
-
-    // print_vec(ray.origin, "Ray ori: ");
-    // print_vec(ray.udir, "Ray dir: ");
-    ray_s_origin = sphere.pos;
-    minus_vec3d(&ray_s_origin, ray.origin);
-    tmp = ray.udir;
-    scale_vec3d(&tmp, -2);
-    b = dot_vec3d(tmp, ray_s_origin);
-    c = dot_vec3d(ray_s_origin, ray_s_origin) - (sphere.r * sphere.r);
-    return ((b * b) - (4 * a * c));
-}
-
-
-bool sphere_hit(t_sphere sphere, t_ray ray, t_vec3d *pos)
+bool sphere_hit(t_sphere sphere, t_ray ray, t_hit *hit)
 {
     float t0;
     float t1;
     float disc;
-    t_vec3d hit;
+    t_vec3d hit1;
     t_vec3d hit2;
-    t_vec3d tmp;
 
     disc = solve_for_hit(ray, sphere, &t0, &t1);
-    if (disc < 0)
+    if (disc < 0 || (t0 < 0 && t1 < 0))
         return false;
-    hit = ray.origin;  
-    tmp = ray.udir;
-    scale_vec3d(&tmp, t0);
-    add_vec3d(&hit, tmp);
-    hit2 = ray.origin;  
-    tmp = ray.udir;
-    scale_vec3d(&tmp, t1);
-    add_vec3d(&hit2, tmp);
-	ft_memset(pos, 0, sizeof(t_vec3d));
+    hit1 = get_point_on_ray(ray, t0);
+    hit2 = get_point_on_ray(ray, t1);  
     if (disc == 0 || t0 < t1)
-        add_vec3d(pos, hit);
+        set_hit(hit1, sphere.color, hit);
     else
-		add_vec3d(pos, hit2);
-
-	// normlize_vec3d(pos);
+        set_hit(hit2, sphere.color, hit);
     return true;
 }
 
