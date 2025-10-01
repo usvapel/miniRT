6794dc52005edc3e531c1d399badb5a1685d0cc0
jackie jan <zakaria.farah55@outlook.com>
2025-10-01 13:11:59 +0300

    added prototypes, wip

--- include/camera.h
+++ include/camera.h
@@ -3,12 +3,22 @@
 
 # include "minirt.h"
 
+# define LEFT 0
+# define RIGHT 1
+
+typedef struct s_engine t_engine;
+
 typedef struct s_camera
 {
 	t_vec3d pos;
 	t_vec3d dir;
+	t_vec3d v;
 	int fov;
 } t_camera;
 
+
+void 	update_camera(void);
+void    look_up_down(t_camera *camera, float dy);
+void    orient_camera(t_engine *engine, float nx, float ny);
 
 #endif
