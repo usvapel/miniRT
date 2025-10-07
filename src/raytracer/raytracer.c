f292b6f1ea71558b55e9573732d7d07de22cfa3e
jackie jan <zakaria.farah55@outlook.com>
2025-10-07 23:26:16 +0300

    checking hits against cylinder

--- src/raytracer/raytracer.c
+++ src/raytracer/raytracer.c
@@ -33,6 +33,7 @@ void	*raytracer(void *thread)
 	t_engine *engine = get_engine();
 	t_sphere *spheres = *engine->objects;
 	t_plane *plane = engine->objects[1];
+	t_cylinder *cy = engine->objects[2];
 	t_threads *t = thread;
 	t_ray ray;
 	t_hit hit;
@@ -53,6 +54,7 @@ void	*raytracer(void *thread)
 				ray = get_ray(x, y);
 				plane_hit(*plane, ray, &hit);
 				sphere_hit(spheres[0], ray, &hit);
+				cylinder_hit(*cy, ray, &hit);
 				if (hit.prev_hit)
 					mlx_put_pixel(engine->image_buffer, x, y, scale_color(&hit.color, 1));
 				else
