f872e78d367c12ab62e00a26d04140619b3e162c
jackie jan <zakaria.farah55@outlook.com>
2025-10-07 23:37:46 +0300

    added prototypes

--- include/geometry.h
+++ include/geometry.h
@@ -20,6 +20,23 @@ typedef struct s_plane
 	t_color color;
 } t_plane;
 
+typedef struct s_cylinder
+{
+	int type;
+	float h;
+	float r;
+	t_vec3d pos;
+	t_vec3d axis;
+	t_color color;
+} t_cylinder;
+
+typedef struct s_circle
+{
+	t_vec3d pos;
+	float r;
+} t_circle;
+
+
 typedef enum e_id
 {
 	AMBIENT_LIGHNING,
@@ -31,6 +48,13 @@ typedef enum e_id
 } t_id ;
 
 bool sphere_hit(t_sphere sphere, t_ray ray, t_hit *hit);
+float solve_for_hit(t_ray ray, t_sphere sphere, float *t0, float *t1);
 bool plane_hit(t_plane plane, t_ray ray, t_hit *hit);
+bool solve_plane_hit(t_plane plane, t_ray ray, float *t);
+void    cylinder_hit(t_cylinder cy, t_ray ray, t_hit *hit);
+
+
+t_circle new_circle(t_vec3d pos, float r);
+bool circle_hit(t_circle circ, t_ray ray, float *t0, float *t1);
 
 #endif
