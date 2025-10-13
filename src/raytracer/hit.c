7326886592656265b13c20d832f87f9cd6fa03f2
Zakaria Farah <zakaria.farah55@outlook.com>
2025-10-13 13:45:32 +0300

    minor refactor

--- src/raytracer/hit.c
+++ src/raytracer/hit.c
@@ -14,20 +14,16 @@ bool    set_hit(t_vec3d new_hit, t_color color, t_hit *hit)
 
 bool closest_hit(t_vec3d pos, t_vec3d hit1, t_vec3d hit2)
 {
-    float pow_mag1;
-    float pow_mag2;
-
-    pow_mag1 = pow_magnitude_vec3d(sub_vec3d(hit1, pos));
-
-    pow_mag2 = pow_magnitude_vec3d(sub_vec3d(hit2, pos));
+    const float pow_mag1 = pow_magnitude_vec3d(sub_vec3d(hit1, pos));
+    const float pow_mag2 = pow_magnitude_vec3d(sub_vec3d(hit2, pos));
     return pow_mag1 < pow_mag2;    
 }
 
 float nearest_t(float t0, float t1)
 {
-    if (t0 > 0 && t1 > 0)
+    if (t0 > 0.0f && t1 > 0.0f)
         return fminf(t0, t1);
-    else if (t0 > 0)
+    else if (t0 > 0.0f)
         return t0;
     else
         return t1;
