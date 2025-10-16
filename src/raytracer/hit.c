230cf62c6c889eb42245ba9bfd00c25e40ee3f7d
Julius Pellinen <julius.pellinen@gmail.com>
2025-10-16 18:22:25 +0300

    norm fixes

--- src/raytracer/hit.c
+++ src/raytracer/hit.c
@@ -1,30 +1,30 @@
 #include "minirt.h"
 
-bool    set_hit(t_vec3d new_hit, t_color color, t_hit *hit)
+bool	set_hit(t_vec3d new_hit, t_color color, t_hit *hit)
 {
-    t_camera cam = get_engine()->camera;
+	const t_camera cam = get_engine()->camera;
 
-   if (hit->prev_hit && closest_hit(cam.pos, hit->pos, new_hit))
-       return false;
-    hit->pos = new_hit;
-    hit->color = color;
-    hit->prev_hit = true;
-    return true;
+	if (hit->prev_hit && closest_hit(cam.pos, hit->pos, new_hit))
+	   return (false);
+	hit->pos = new_hit;
+	hit->color = color;
+	hit->prev_hit = true;
+	return (true);
 }
 
-bool closest_hit(t_vec3d pos, t_vec3d hit1, t_vec3d hit2)
+bool	closest_hit(t_vec3d pos, t_vec3d hit1, t_vec3d hit2)
 {
-    const float pow_mag1 = pow_magnitude_vec3d(sub_vec3d(hit1, pos));
-    const float pow_mag2 = pow_magnitude_vec3d(sub_vec3d(hit2, pos));
-    return pow_mag1 < pow_mag2;    
+	const float pow_mag1 = pow_magnitude_vec3d(sub_vec3d(hit1, pos));
+	const float pow_mag2 = pow_magnitude_vec3d(sub_vec3d(hit2, pos));
+	return (pow_mag1 < pow_mag2);
 }
 
-float nearest_t(float t0, float t1)
+float	nearest_t(float t0, float t1)
 {
-    if (t0 > 0.0f && t1 > 0.0f)
-        return fminf(t0, t1);
-    else if (t0 > 0.0f)
-        return t0;
-    else
-        return t1;
+	if (t0 > 0.0f && t1 > 0.0f)
+		return (fminf(t0, t1));
+	else if (t0 > 0.0f)
+		return (t0);
+	else
+		return (t1);
 }
