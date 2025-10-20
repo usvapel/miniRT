a40566e2cb8b8ebcf8750f7099fac74d8963e168
jackie jan <zakaria.farah55@outlook.com>
2025-10-20 12:43:50 +0300

    Minor changes

--- src/geometry/light.c
+++ src/geometry/light.c
@@ -1,7 +1,7 @@
 
 #include "minirt.h"
 
-bool light_hit(t_light light, t_ray ray, t_hit *hit)
+bool light_hit(t_light *light, t_ray ray, t_hit *hit)
 {
 	float t0;
 	float t1;
@@ -9,14 +9,14 @@ bool light_hit(t_light light, t_ray ray, t_hit *hit)
 	t_vec3d n_hit_pos;
 	bool is_set = {0};
 
-	disc = solve_sphere_hit(ray, new_sphere(light.pos, light.r), &t0, &t1);
+	disc = solve_sphere_hit(ray, new_sphere(light->base.pos, light->r), &t0, &t1);
 	if (disc < 0.0f || (t0 < 0 && t1 < 0))
 		return false;
 	n_hit_pos = get_point_on_ray(ray, nearest_t(t0, t1));
-    is_set = set_hit(n_hit_pos, light.color, hit);
+    is_set = set_hit(light, n_hit_pos, ray, hit);
 	if (!is_set)
 		return false;
-	hit->color = light.color;
+	hit->color = light->base.color;
 	hit->type = LIGHT;
     return true;
 }
