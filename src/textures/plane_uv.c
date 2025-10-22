c56b42c44069095754ab903ed44c410156fff686
Zakaria Farah <zakaria.farah55@outlook.com>
2025-10-22 18:44:56 +0300

    working checkerboard textures for sphere and plane

--- src/textures/plane_uv.c
+++ src/textures/plane_uv.c
@@ -13,23 +13,13 @@ void    plane_uv(t_plane plane, t_hit *hit)
     if (plane.base.texture.type == CHECKERBOARD)
         lchecker_board(hit, plane.base.texture.index, l_hit.x, l_hit.z);
 }
-void    sphere_uv(t_sphere sphere, t_hit *hit)
-{
-    t_vec3d n = normalize_vec3d(sub_vec3d(hit->pos, sphere.base.pos));
-    float u = atan2(n.x, n.z) / (2*3.147) + 0.5;
-    float v = n.y * 0.5 + 0.5;
-
-    if (sphere.base.texture.type == CHECKERBOARD)
-        lchecker_board(hit, sphere.base.texture.index, u, v);
-}
 
 void lchecker_board(t_hit *hit, int texture_index, float u, float v)
 {
-    float checker_size = 0.09f;
     t_vector *checkers = get_engine()->textures.checkers;
 	t_checker *checkboard = (t_checker *)(checkers->data[texture_index]);
-	int i_u = floorf(u / checker_size);
-	int i_v = floorf(v / checker_size);
+	int i_u = floorf(u / checkboard->block_size);
+	int i_v = floorf(v / checkboard->block_size);
 	t_color color = ((i_u + i_v) % 2 == 0) ? checkboard->color1 : checkboard->color2;
     hit->color = color;
 }
