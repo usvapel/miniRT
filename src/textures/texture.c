6a52eb991cbea5da0efa2315465afb7194a14c18
Zakaria Farah <zakaria.farah55@outlook.com>
2025-10-22 18:44:56 +0300

    working checkerboard textures for sphere and plane

--- src/textures/texture.c
+++ src/textures/texture.c
@@ -3,6 +3,8 @@
 void    apply_texture(t_hit *hit)
 {
     t_object *base = (t_object *)hit->obj;
+    if (base->texture.index < 0)
+        return;
     if (base->type == PLANE)
         plane_uv(*((t_plane *)hit->obj), hit);
     else if (base->type == SPHERE)
