10c11ce4e23c6ae29de7a1aa35eeee59d1b68198
usva <julius.pellinen@gmail.com>
2025-10-24 11:20:22 -0500

    comment
    if the img is missing the index, do we care?

--- src/parsing/parse_plane.c
+++ src/parsing/parse_plane.c
@@ -54,7 +54,7 @@ void	init_plane(t_engine *engine, char **split)
 	plane->normal = parse_vec3d(v, v->data[1]);
 	plane->base.color = parse_color(v, v->data[2]);
 	plane->base.texture.index = -1;
-	additional_values(v, &plane->base, split, 3);
+	additional_values(v, &plane->base, split, 3); // make sure index is correct
 	free_vector(v);
 	validate_color(plane->base.color);
 	validate_normal(plane->normal);
