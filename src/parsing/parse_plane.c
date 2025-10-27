824689fcdd447a9a9570c6da273673098d8628d7
Julius Pellinen <julius.pellinen@gmail.com>
2025-10-27 17:10:29 +0200

    norm fixes

--- src/parsing/parse_plane.c
+++ src/parsing/parse_plane.c
@@ -10,34 +10,11 @@ static void	validate_normal(t_vec3d normal)
 		runtime_error("Invalid normal (-1 - 1)");
 }
 
-void	get_additional_values(t_vector *v, t_object *base, char **split, int index)
-{
-	while (split[index])
-	{
-		printf("%d %s\n", index, split[index]);
-		if (ft_strcmp(split[index], "ch") == 0 && split[index + 1])
-			link_texture(base, split + index);
-		else if (ft_strcmp(split[index], "img") == 0 && split[index + 1])
-			link_texture(base, split + index);
-		else if (ft_strcmp(split[index], "rl") == 0 && split[index + 1])
-		{
-			base->material.reflec = ft_atof(split[index + 1]);
-			if (base->material.reflec > 1.0f || base->material.reflec < 0.0f)
-			{
-				free_vector(v);
-				runtime_error("invalid reflection value! (0 - 1)");
-			}
-		}
-		index++;
-	}
-}
-
 void	init_plane(t_engine *engine, char **split)
 {
 	t_plane		*plane;
 	t_vector	*v;
 
-	puts("parse plane");
 	v = new_vector(1);
 	if (!v)
 		runtime_error("allocation failed");
@@ -56,7 +33,7 @@ void	init_plane(t_engine *engine, char **split)
 	plane->normal = parse_vec3d(v, v->data[1]);
 	plane->base.color = parse_color(v, v->data[2]);
 	plane->base.texture.index = -1;
-	get_additional_values(v, &plane->base, split, 4); // make sure index is correct
+	get_additional_values(v, &plane->base, split, 4);
 	free_vector(v);
 	validate_color(plane->base.color);
 	validate_normal(plane->normal);
