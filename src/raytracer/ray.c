ee0d544ebf66734ecdbafb01d259457139c4578f
jackie jan <zakaria.farah55@outlook.com>
2025-10-29 15:56:49 +0200

    added helper func

--- src/raytracer/ray.c
+++ src/raytracer/ray.c
@@ -24,3 +24,12 @@ t_vec3d	get_point_on_ray(t_ray ray, float t)
 	add_vec3d(&p, tmp);
 	return (p);
 }
+
+t_ray local_ray(t_ray ray, t_basis3d local, t_vec3d local_origin)
+{
+    t_ray r;
+
+    r.origin = point_in_basis(ray.origin, local, local_origin);
+    r.udir = vec_in_basis(ray.udir, local);
+    return r;
+}
