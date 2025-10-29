179fdbfec6d4b709b11e51c68af0ea8b51533c92
jackie jan <zakaria.farah55@outlook.com>
2025-10-29 15:31:36 +0200

    By default lights without object attatchment don't register hit

--- src/geometry/light.c
+++ src/geometry/light.c
@@ -1,22 +1,66 @@
 
 #include "minirt.h"
 
-bool light_hit(t_light *light, t_ray ray, t_hit *hit)
+bool light_hit(t_generic_light *g_light, t_ray ray, t_hit *hit)
 {
-	float t0;
-	float t1;
-	float disc;
-	t_vec3d n_hit_pos;
-	bool is_set = {0};
+	if (!g_light->obj)
+		return false;
+	return obj_intersection(g_light->obj, ray, hit);
+}
 
-	disc = solve_sphere_hit(ray, new_sphere(light->base.pos, light->r), &t0, &t1);
-	if (disc < 0.0f || (t0 < 0 && t1 < 0))
+bool spot_light_hit(t_generic_light *g_spot, t_hit *hit, t_phong *phong)
+{
+	t_spot_light *spot = (t_spot_light *)g_spot->light;
+	t_plane p = new_plane(hit->pos, phong->normal);
+	t_object *base;
+	t_ray lray;
+	float t;
+	float r;
+	float r1;
+	float pos_hit;
+	t_vec3d pos;
+	if (!g_spot->obj)
+		base = get_base_object(g_spot);
+	else
+		base = get_base_object(g_spot->obj);
+	lray.origin = adjusted_light_pos(*g_spot);
+	lray.udir = *base->axis;
+	r = tanf(deg_to_radians(spot->fov)) * spot->range;
+	if (!solve_plane_hit(p, lray, &t))
 		return false;
-	n_hit_pos = get_point_on_ray(ray, nearest_t(t0, t1));
-    is_set = set_hit(light, n_hit_pos, ray, hit);
-	if (!is_set)
+	pos = get_point_on_ray(lray, t);
+	r1 = tanf(deg_to_radians(spot->fov)) * magnitude_vec3d(sub_vec3d(lray.origin, pos));
+	pos_hit = magnitude_vec3d(sub_vec3d(pos, hit->pos));
+	if (pos_hit > r1 || r1 > r)	
 		return false;
-	hit->color = light->base.color;
-	hit->type = LIGHT;
-    return true;
+	// scale_vec3d(&phong->light_color, r / r1);
+	return true;
+}
+
+// t_vec3d	opague_by_distance(t_vec3d color, float d, float d_max)
+// {
+// 	t_color col = vec3d_to_color(color);
+	
+// 	col.a *= d_max / d;
+// 	return color_to_vec3d();
+// }
+t_vec3d	adjusted_light_pos(t_generic_light light)
+{
+	t_object obj_base;
+	t_ray r;
+	float offset;
+
+	if (!light.obj)
+		return light.base.pos;
+	obj_base = *get_base_object(light.obj);
+	r.origin = obj_base.pos;
+	r.udir = *obj_base.axis;
+	offset = 0.0f;
+	if (obj_base.type == SPHERE)
+		offset = ((t_sphere *)light.obj)->r;
+	else if (obj_base.type == CYLINDER)
+		offset = ((t_cylinder *)light.obj)->h / 2;
+	else if (obj_base.type == PARABOLOID)
+		offset = ((t_paraboloid *)light.obj)->h;
+	return get_point_on_ray(r, offset + 2 * 1e-2);
 }
