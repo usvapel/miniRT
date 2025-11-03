2a0f58b924911b7496bf08e5292ed8b6fcb64c47
Julius Pellinen <julius.pellinen@gmail.com>
2025-11-03 15:11:22 +0200

    better object movement

--- src/MLX/move_objects.c
+++ src/MLX/move_objects.c
@@ -28,12 +28,7 @@ void move_objects(t_engine *engine)
 	static t_ray ray;
 	static void *obj = NULL;
 	static bool grabbed = false;
-	float dy;
-	float dx;
 	int type;
-
-	dy = engine->mouse.prev_pos.y - engine->mouse.pos.y;
-	dx = engine->mouse.prev_pos.x - engine->mouse.pos.x;
 
 	if (mlx_is_mouse_down(engine->mlx, MLX_MOUSE_BUTTON_LEFT))
 	{
@@ -44,42 +39,13 @@ void move_objects(t_engine *engine)
 				return ;
 			grabbed = true;
 		}
-		move_object(obj, ray, dx, dy);
+		move_object(obj, engine->mouse.pos.x, engine->mouse.pos.y);
 	}
 	else
 		grabbed = false;
 }
 
-void *inside_object(t_ray *ray, double x, double y, int *type)
-{
-	t_engine *engine = get_engine();
-	*ray = get_ray(x, y);
-	t_hit hit = {0};
-
-	objects_intersection(engine, ray, &hit);
-	if (!hit.prev_hit)
-		return (NULL);
-	*type = hit.type;
-	return (hit.obj);
-}
-
-static void move_object(void *obj, t_ray ray, float dx, float dy)
-{
-	t_object *base = (t_object*)obj;
-	t_vec3d tmp = base->pos;
-	t_vec3d b = {0,1.0,0};
-	long dt = get_engine()->frame.delta;
-	
-	minus_vec3d(&tmp, ray.origin);
-	dy *= magnitude_vec3d(tmp) * OBJ_SPEED * dt;
-	dx *= magnitude_vec3d(tmp) * OBJ_SPEED * dt;
-	move_pos_left_right(&get_engine()->camera, &base->pos, dx);
-	scale_vec3d(&b, dy);
-	add_vec3d(&base->pos, b);
-	update_camera();
-}
-
-void rotate_object(void *obj,  t_ray ray, float dx, float dy)
+void rotate_object(void *obj, float dx, float dy)
 {
 	t_object *base = get_base_object(obj);
 	t_vec3d tmp = base->pos;
@@ -124,16 +90,4 @@ void rotate_objects(t_engine *engine)
 	}
 	else
 		grabbed = false;
-}
-
-static void    move_pos_left_right(t_camera *cam, t_vec3d *pos, float d)
-{
-    t_vec3d norm;
-    float dz = cam->dir.z;
-    float dx = cam->dir.x;
-    norm.y = 0;
-    norm.x = -dz;
-    norm.z = dx;
-    scale_vec3d(&norm, d);
-    add_vec3d(pos, norm);
 }
