f4aaf93fe46c589c3ab3409f9be802237d3a8b33
Julius Pellinen <julius.pellinen@gmail.com>
2025-11-03 21:35:45 +0200

    refactored some light parse logic to get rid of leaks...  . . .dasd!!!

--- src/parsing/parse_light.c
+++ src/parsing/parse_light.c
@@ -1,18 +1,13 @@
 #include "minirt.h"
 
-static void	validate_light(t_generic_light g_light)
-{
-	if (g_light.brightness < 0.0f || g_light.brightness > 1.0f)
-		runtime_error("Invalid light brightness value (0-1)");
-}
-
-static void	set_values(t_generic_light *g_light, t_point_light *light, t_vector *v, char **split)
+static void	set_values(t_generic_light *g_light, t_point_light *light,
+		t_vector *v, char **split)
 {
 	add_elem(v, safe_split(v, split[1]));
 	add_elem(v, safe_split(v, split[2]));
 	add_elem(v, safe_split(v, split[3]));
 	g_light->base.type = POINT_LIGHT;
-	light->axis = new_vec3d(0,1,0);
+	light->axis = new_vec3d(0, 1, 0);
 	g_light->base.pos = parse_vec3d(v, v->data[0]);
 	g_light->brightness = ft_atof(((char ***)v->data)[1][0]);
 	g_light->base.color = parse_color(v, v->data[2]);
@@ -25,33 +20,30 @@ static void	set_values(t_generic_light *g_light, t_point_light *light, t_vector 
 	if (v->count == 4)
 		light->r = ft_atof(((char ***)v->data)[3][0]);
 	get_additional_values(v, (void *)g_light, split, 4);
+	if (g_light->brightness < 0.0f || g_light->brightness > 1.0f)
+		runtime_error("Invalid light brightness value (0-1)");
 }
 
-void	init_light(t_vector *lights, char **split)
+void	init_light(t_vector *g_lights, char **split)
 {
-	t_generic_light *g_light;
-	t_point_light	*light;
-	t_vector *v;
+	t_generic_light	*g_light;
+	t_vector		*v;
 
 	puts("parse point light");
 	g_light = ft_calloc(1, sizeof(t_generic_light));
 	if (!g_light)
 		runtime_error("failure during memory allocation!");
-	add_elem(lights, g_light);
-	light = ft_calloc(1, sizeof(t_point_light));
-	if (!light)
-		runtime_error("failure during memory allocation!");
-	g_light->light = (void *)light;
 	v = new_vector(1);
 	if (!v)
 		runtime_error("allocation failed");
-	set_values(g_light, light, v, split);
-	free_vector(v);
+	set_values(g_light, &g_light->point_light, v, split);
+	free_split_vector(v);
 	validate_color(g_light->base.color);
-	validate_light(*g_light);
+	add_elem(g_lights, g_light);
 }
 
-static void	set_values2(t_generic_light *g_light, t_spot_light *light, t_vector *v, char **split)
+static void	set_values2(t_generic_light *g_light, t_spot_light *light,
+		t_vector *v, char **split)
 {
 	add_elem(v, safe_split(v, split[1]));
 	add_elem(v, safe_split(v, split[2]));
@@ -73,29 +65,24 @@ static void	set_values2(t_generic_light *g_light, t_spot_light *light, t_vector 
 	g_light->base.texture.index = -1;
 	g_light->obj_index = -1;
 	get_additional_values(v, (void *)g_light, split, 7);
+	if (g_light->brightness < 0.0f || g_light->brightness > 1.0f)
+		runtime_error("Invalid light brightness value (0-1)");
 }
 
-void	init_spot_light(t_vector *lights, char **split)
+void	init_spot_light(t_vector *g_lights, char **split)
 {
-	t_generic_light *g_light;
-	t_spot_light	*light;
-	t_vector *v;
+	t_generic_light	*g_light;
+	t_vector		*v;
 
 	puts("parse spot light");
-	g_light = ft_calloc(1,sizeof(t_generic_light));
+	g_light = ft_calloc(1, sizeof(t_generic_light));
 	if (!g_light)
 		runtime_error("failure during memory allocation!");
-	add_elem(lights, g_light);
-	light = ft_calloc(1, sizeof(t_spot_light));
-	if (!light)
-		runtime_error("failure during memory allocation!");
-	g_light->light = light;
 	v = new_vector(1);
 	if (!v)
 		runtime_error("allocation failed");
-	set_values2(g_light, light, v, split);
-	free_vector(v);
+	set_values2(g_light, &g_light->spot_light, v, split);
+	free_split_vector(v);
 	validate_color(g_light->base.color);
-	validate_light(*g_light);
-	add_elem(lights, g_light);
+	add_elem(g_lights, g_light);
 }
