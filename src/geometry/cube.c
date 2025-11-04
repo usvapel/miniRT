90ec667c2284f1c557a2be014cdde3e5388a7a74
jackie jan <zakaria.farah55@outlook.com>
2025-11-04 14:53:50 +0200

    minor fixes

--- src/geometry/cube.c
+++ src/geometry/cube.c
@@ -1,8 +1,6 @@
 #include "minirt.h"
 
 static t_vec3d hit_normal(t_ray ray, int axis);
-static  void set_cube_min_max(t_cube *cube, float h, float w);
-static t_cube new_cube(float h, float w);
 
 bool    aabb_intersection(t_cube cube, t_ray ray, float *tnear, int *axis)
 {
@@ -78,25 +76,3 @@ static t_vec3d hit_normal(t_ray ray, int axis)
     return normal;
 }
 
-static  void set_cube_min_max(t_cube *cube, float h, float w)
-{
-    const t_vec3d u = new_vec3d(w, 0, 0);
-    const t_vec3d v = new_vec3d(0, h, 0);
-    const t_vec3d z = new_vec3d(0,0, sqrtf(w * w + h * h));
-
-    cube->min = add2_vec3d(cube->base.pos, nscale_vec3d(u, -0.5));
-    cube->min = add2_vec3d(cube->min, nscale_vec3d(v, -0.5));
-    cube->min = add2_vec3d(cube->min, nscale_vec3d(z, -0.5));
-
-    cube->max = add2_vec3d(cube->base.pos, nscale_vec3d(u, 0.5));
-    cube->max = add2_vec3d(cube->max, nscale_vec3d(v, 0.5));
-    cube->max = add2_vec3d(cube->max, nscale_vec3d(z, 0.5)); 
-
-}
-
-static t_cube new_cube(float h, float w)
-{
-    t_cube cube = {0};
-    set_cube_min_max(&cube, h, w);
-    return cube;
-}
