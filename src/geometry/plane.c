ce617a95d5d1459a961cc21ed6903a6150d375f7
Julius Pellinen <julius.pellinen@gmail.com>
2025-11-04 19:03:15 +0200

    restored plane.c

--- src/geometry/plane.c
+++ src/geometry/plane.c
@@ -1,56 +1,59 @@
 #include "minirt.h"
-#include "primitives.h"
-/*
-Point in ray: p = o + td, o is origin, d ray direction and t distance from origin
-Plane in normal form: n * (r-r0) = 0, n normal form, r0 known point and r is any point in the plane, r == p
---> n * (o + td -r0) = 0
---> t = n*(r0 - o) / (d * n), if d * n == 0, ray is inside the plane,  n*(r0 - o) == 0 no solution
-*/
-bool plane_hit(t_plane *plane, t_ray ray, t_hit *hit)
-{
-    const float d_dot_n = dot_vec3d(plane->normal, ray.udir);
-    t_vec3d tmp;
-    float t;
-	t_vec3d pos;
 
-        return false;
-    tmp = plane->base.pos; 
-    minus_vec3d(&tmp, ray.origin);
-    t = dot_vec3d(plane->normal, tmp);
-    t /= d_dot_n;
-    if (t < 0.0f)
-		return false;
+/*
+Point in ray: p = o + td, o is origin,
+	d ray direction and t distance from origin
+Plane in normal form: n * (r-r0) = 0, n normal form,
+	r0 known point and r is any point in the plane, r == p
+--> n * (o + td -r0) = 0
+--> t = n*(r0 - o) / (d * n), if d * n == 0, ray is inside the plane,  n*(r0
+		- o) == 0 no solution
+*/
+bool	plane_hit(t_plane *plane, t_ray ray, t_hit *hit)
+{
+	const float	d_dot_n = dot_vec3d(plane->normal, ray.udir);
+	t_vec3d		tmp;
+	float		t;
+	t_vec3d		pos;
+
 	if (fabsf(d_dot_n) == (float)EPSILON)
+		return (false);
+	tmp = plane->base.pos;
+	minus_vec3d(&tmp, ray.origin);
+	t = dot_vec3d(plane->normal, tmp);
+	t /= d_dot_n;
+	if (t < 0.0f)
+		return (false);
 	pos = get_point_on_ray(ray, t);
 	if (!set_hit(plane, pos, ray, hit))
-        return false;
-    hit->normal = plane->normal;
-    hit->type = PLANE;
+		return (false);
+	hit->normal = plane->normal;
+	hit->type = PLANE;
 	hit->material = plane->base.material;
-    return true;
+	return (true);
 }
 
-bool solve_plane_hit(t_plane plane, t_ray ray, float *t)
+bool	solve_plane_hit(t_plane plane, t_ray ray, float *t)
 {
-    const float d_dot_n = dot_vec3d(plane.normal, ray.udir);
-    t_vec3d tmp;
+	const float	d_dot_n = dot_vec3d(plane.normal, ray.udir);
+	t_vec3d		tmp;
 
-        return false;
-    tmp = plane.base.pos; 
-    minus_vec3d(&tmp, ray.origin);
-    *t = dot_vec3d(plane.normal, tmp);
-    *t /= d_dot_n;
-    if (*t < 0.0f)
-        return false;
-    return true;
 	if (fabsf(d_dot_n) == (float)EPSILON)
+		return (false);
+	tmp = plane.base.pos;
+	minus_vec3d(&tmp, ray.origin);
+	*t = dot_vec3d(plane.normal, tmp);
+	*t /= d_dot_n;
+	if (*t < 0.0f)
+		return (false);
+	return (true);
 }
 
-t_plane new_plane(t_vec3d pos, t_vec3d n)
+t_plane	new_plane(t_vec3d pos, t_vec3d n)
 {
-    t_plane p;
-    
-    p.normal = n;
-    p.base.pos = pos;
-    return p;
+	t_plane p;
+
+	p.normal = n;
+	p.base.pos = pos;
+	return (p);
 }
