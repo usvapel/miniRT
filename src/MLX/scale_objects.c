24abb6d9326d5eace35d033535b68399c38c0df6
jackie jan <zakaria.farah55@outlook.com>
2025-11-04 14:58:57 +0200

    Refactored scaling objects

--- src/MLX/scale_objects.c
+++ src/MLX/scale_objects.c
@@ -1,64 +1,35 @@
 #include "minirt.h"
-static void scale_by_factor(void *object, float dx, float dy);
-static void    scale_object(void *obj, double x, double y);
+
+static void    scale_object(void *obj);
 
 void scale_objects(t_engine *engine)
 {
 	t_ray ray;
 	int type;
+	
 	if (mlx_is_mouse_down(engine->mlx, MLX_MOUSE_BUTTON_LEFT))
 	{
 		void *obj = inside_object(&ray, engine->mouse.pos.x, engine->mouse.pos.y, &type);
 		if (!obj)
 			return ;
-		scale_object(obj, engine->mouse.pos.x, engine->mouse.pos.y);
+		scale_object(obj);
 		update_camera();
 	}
 }
 
-static void scale_by_factor(void *object, float dx, float dy)
+static void    scale_object(void *obj)
 {
-	int type = *(int *)object;
-	const long dt = get_engine()->frame.delta;
+	const t_engine *engine = get_engine();
+	const float dx = engine->mouse.prev_pos.x - engine->mouse.pos.x;
+	const float dy = engine->mouse.prev_pos.y - engine->mouse.pos.y;
+	const t_object *base = get_base_object(obj);
 
-	if (type == SPHERE)
-	{
-		(*(t_sphere *)object).r += OBJ_S_SPEED * -dx * dt;
-		if ((*(t_sphere *)object).r <= 0.05f)
-			(*(t_sphere *)object).r = 0.05f;
-	}
-	if (type == CYLINDER)
-	{
-		(*(t_cylinder *)object).r += OBJ_S_SPEED * -dx * dt;
-		(*(t_cylinder *)object).h += OBJ_S_SPEED * dy * dt;
-		if ((*(t_cylinder *)object).r <= 0.05f)
-			(*(t_cylinder *)object).r = 0.05f;
-		if ((*(t_cylinder *)object).h <= 0.05f)
-			(*(t_cylinder *)object).h = 0.05f;
-	}
-	if (type == LIGHT)
-	{
-		(*(t_light *)object).r += OBJ_S_SPEED * -dx * dt;
-		if ((*(t_light *)object).r <= 0.05f)
-			(*(t_light *)object).r = 0.05f;
-	}
-	if (type == PARABOLOID)
-	{
-		(*(t_paraboloid *)object).focal += OBJ_S_SPEED * -dx * dt;
-		(*(t_paraboloid *)object).h += OBJ_S_SPEED * dy * dt;
-		if ((*(t_paraboloid *)object).focal <= 0.05f)
-			(*(t_paraboloid *)object).focal = 0.05f;
-		if ((*(t_paraboloid *)object).h <= 0.05f)
-			(*(t_paraboloid *)object).h = 0.05f;
-	}
-}
-
-static void    scale_object(void *obj, double x, double y)
-{
-	t_mouse mouse = get_engine()->mouse;
-	(void)x;
-	(void)y;
-	float 			dx = mouse.prev_pos.x - mouse.pos.x;
-	float 			dy = mouse.prev_pos.y - mouse.pos.y;
-	scale_by_factor(obj, dx, dy);
+	if (base->type == CUBE)
+		scale_cube((t_cube *)obj, dx, dy, engine->frame.delta);
+	else if (base->type == SPHERE)
+		scale_sphere((t_sphere *)obj, dx, engine->frame.delta);
+	else if (base->type == CYLINDER)
+		scale_cylinder((t_cylinder *)obj, dx, dy, engine->frame.delta);
+	else if (base->type == PARABOLOID)
+		scale_paraboloid((t_paraboloid *)obj, dx, dy, engine->frame.delta);
 }
