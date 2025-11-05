53fb0279b6098cc3af0391ffc63f8c0c38e1e9ff
jackie jan <zakaria.farah55@outlook.com>
2025-11-05 19:56:20 +0200

    refactored light hit

--- src/geometry/light.c
+++ src/geometry/light.c
@@ -10,39 +10,28 @@ bool light_hit(t_generic_light *g_light, t_ray ray, t_hit *hit)
 
 bool spot_light_hit(t_generic_light *g_spot, t_hit *hit, t_phong *phong)
 {
-	t_plane p = new_plane(hit->pos, phong->normal);
-	t_object *base = get_base_light(g_spot);
 	t_ray lray;
-	float t;
+	float t_h[2];
 	float r1;
 	float pos_hit;
 	t_vec3d pos;
-	float h1;
-	t_spot_light	*spot;
 
-	spot = &g_spot->spot_light;
 	lray.origin = adjusted_light_pos(*g_spot);
-	lray.udir = *base->axis;
-	if (!solve_plane_hit(p, lray, &t))
+	lray.udir = *get_base_light(g_spot)->axis;
+	if (!solve_plane_hit(new_plane(hit->pos, phong->normal), lray, &t_h[0]))
 		return false;
-	pos = get_point_on_ray(lray, t);
-	r1 = tanf(deg_to_radians(spot->fov)) * magnitude_vec3d(sub_vec3d(lray.origin, pos));
-	h1 = magnitude_vec3d(sub_vec3d(lray.origin, pos));
+	pos = get_point_on_ray(lray, t_h[0]);
+	r1 = tanf(deg_to_radians(g_spot->spot_light.fov)) * magnitude_vec3d(sub_vec3d(lray.origin, pos));
+	t_h[1] = magnitude_vec3d(sub_vec3d(lray.origin, pos));
 	pos_hit = magnitude_vec3d(sub_vec3d(pos, hit->pos));
 	if (pos_hit > r1)	
 		return false;
 	scale_vec3d(&phong->light_color, smoothstep2(1 - pos_hit / r1, 0.5));
-	scale_vec3d(&phong->light_color,smoothstep(1 - h1 / spot->range));
+	scale_vec3d(&phong->light_color,smoothstep(1 - t_h[1] / g_spot->spot_light.range));
+	phong->light_color = color_to_vec3d(vec3d_to_color(phong->light_color));
 	return true;
 }
 
-// t_vec3d	opague_by_distance(t_vec3d color, float d, float d_max)
-// {
-// 	t_color col = vec3d_to_color(color);
-	
-// 	col.a *= d_max / d;
-// 	return color_to_vec3d();
-// }
 t_vec3d	adjusted_light_pos(t_generic_light light)
 {
 	t_object obj_base;
