568366d89e4b9c75820a68fc79f1f0735021f1d6
Zakaria Farah <zfarah@c2r5p7.hive.fi>
2025-11-05 22:16:56 +0200

    wip

--- src/geometry/cylinder.c
+++ src/geometry/cylinder.c
@@ -1,100 +1,101 @@
 #include "geometry.h"
-#include "minirt.h"
 #include "math.h"
+#include "minirt.h"
 #include "primitives.h"
 
-static bool    cylinder_disk_hit(t_cylinder cy, t_ray ray, t_hit *hit, int top);
-static bool cylinder_body_hit(t_cylinder cy, t_ray ray, t_hit *hit);
-static bool is_body_hit(t_cylinder cy, t_ray ray, t_hit *hit, float t);
+static bool	cylinder_disk_hit(t_cylinder cy, t_ray ray, t_hit *hit, int top);
+static bool	cylinder_body_hit(t_cylinder cy, t_ray ray, t_hit *hit);
+static bool	is_body_hit(t_cylinder cy, t_ray ray, t_hit *hit, float t);
 
-bool    cylinder_hit(t_cylinder *cy, t_ray ray, t_hit *hit)
+bool	cylinder_hit(t_cylinder *cy, t_ray ray, t_hit *hit)
 {
-    t_hit local_hit;
-    bool hit_is_set;
+	t_hit	local_hit;
+	bool	hit_is_set;
 
-    hit_is_set = false;
-    ft_memset(&local_hit, 0, sizeof(t_hit));
-    if (cylinder_disk_hit(*cy, ray, &local_hit, 1))
-        hit_is_set = set_hit(cy, local_hit.pos, ray, hit);
-    if (cylinder_disk_hit(*cy, ray, &local_hit, -1))
-        hit_is_set = set_hit(cy, local_hit.pos, ray, hit);
-    if (cylinder_body_hit(*cy, ray, &local_hit))
-        hit_is_set = set_hit(cy, local_hit.pos, ray, hit);
-    if (hit_is_set)
-    {
-        hit->color = cy->base.color;
-        hit->normal = local_hit.normal;
-        hit->pos = local_hit.pos;
-        hit->type = CYLINDER;
+	hit_is_set = false;
+	ft_memset(&local_hit, 0, sizeof(t_hit));
+	if (cylinder_disk_hit(*cy, ray, &local_hit, 1))
+		hit_is_set = set_hit(cy, local_hit.pos, ray, hit);
+	if (cylinder_disk_hit(*cy, ray, &local_hit, -1))
+		hit_is_set = set_hit(cy, local_hit.pos, ray, hit);
+	if (cylinder_body_hit(*cy, ray, &local_hit))
+		hit_is_set = set_hit(cy, local_hit.pos, ray, hit);
+	if (hit_is_set)
+	{
+		hit->color = cy->base.color;
+		hit->normal = local_hit.normal;
+		hit->pos = local_hit.pos;
+		hit->type = CYLINDER;
 		hit->material = cy->base.material;
-		return true;
-    }
-    return false;
+		return (true);
+	}
+	return (false);
 }
 
-static bool    cylinder_disk_hit(t_cylinder cy, t_ray ray, t_hit *hit, int top)
+static bool	cylinder_disk_hit(t_cylinder cy, t_ray ray, t_hit *hit, int top)
 {
-    t_plane p;
-    t_vec3d lhit;
-    float magni;
-    t_vec3d tmp;
-    float t;
+	t_plane	p;
+	t_vec3d	lhit;
+	float	magni;
+	t_vec3d	tmp;
+	float	t;
 
-    ft_memset(&lhit, 0, sizeof(t_vec3d));
-    p.base.color = cy.base.color;
-    p.base.pos = cy.base.pos;
-    p.normal = cy.axis;
-    add_vec3d(&p.base.pos, nscale_vec3d(cy.axis, top * cy.h * 0.5));
-    if (!solve_plane_hit(p, ray, &t))
-        return false;
-    if (t < 0)
-        return false;
-    lhit = get_point_on_ray(ray, t);
-    tmp = lhit;
-    minus_vec3d(&tmp, p.base.pos);
-    magni = magnitude_vec3d(tmp);
-    if (magni > cy.r)
-        return false;
-    if (!set_hit(NULL, lhit, ray, hit))
-        return false;
-    hit->normal = nscale_vec3d(cy.axis, top);
-    return true; 
+	ft_memset(&lhit, 0, sizeof(t_vec3d));
+	p.base.color = cy.base.color;
+	p.base.pos = cy.base.pos;
+	p.normal = cy.axis;
+	add_vec3d(&p.base.pos, nscale_vec3d(cy.axis, top * cy.h * 0.5));
+	if (!solve_plane_hit(p, ray, &t))
+		return (false);
+	if (t < 0)
+		return (false);
+	lhit = get_point_on_ray(ray, t);
+	tmp = lhit;
+	minus_vec3d(&tmp, p.base.pos);
+	magni = magnitude_vec3d(tmp);
+	if (magni > cy.r)
+		return (false);
+	if (!set_hit(NULL, lhit, ray, hit))
+		return (false);
+	hit->normal = nscale_vec3d(cy.axis, top);
+	return (true);
 }
 
-
-static bool cylinder_body_hit(t_cylinder cy, t_ray ray, t_hit *hit)
+static bool	cylinder_body_hit(t_cylinder cy, t_ray ray, t_hit *hit)
 {
-    const t_basis3d cy_local = build_local_basis(cy.axis);
-    const t_vec3d origin = sub_vec3d(cy.base.pos, nscale_vec3d(cy.axis, cy.h * 0.5));
-    float t0;
-    float t1;
-    t_ray lray;
+	const t_basis3d	cy_local = build_local_basis(cy.axis);
+	const t_vec3d	origin = sub_vec3d(cy.base.pos, nscale_vec3d(cy.axis, cy.h
+				* 0.5));
+	float			t0;
+	float			t1;
+	t_ray			lray;
 
-    lray = local_ray(ray, cy_local, origin);
-    lray.udir.y = 0;
-    lray.origin.y = 0;
-    if (!circle_hit(new_circle(new_vec3d(0,0,0), cy.r), lray, &t0, &t1))
-        return false;
-    if (t0 < 0 && t1 < 0)
-        return false;
-    return (is_body_hit(cy, ray, hit, nearest_t(t0, t1)));
+	lray = local_ray(ray, cy_local, origin);
+	lray.udir.y = 0;
+	lray.origin.y = 0;
+	if (!circle_hit(new_circle(new_vec3d(0, 0, 0), cy.r), lray, &t0, &t1))
+		return (false);
+	if (t0 < 0 && t1 < 0)
+		return (false);
+	return (is_body_hit(cy, ray, hit, nearest_t(t0, t1)));
 }
 
-static bool is_body_hit(t_cylinder cy, t_ray ray, t_hit *hit, float t)
+static bool	is_body_hit(t_cylinder cy, t_ray ray, t_hit *hit, float t)
 {
-    const t_basis3d local = build_local_basis(cy.axis);
-    const t_vec3d origin = sub_vec3d(cy.base.pos, nscale_vec3d(cy.axis, cy.h * 0.5));
-    const t_ray lray = local_ray(ray, local, origin);
-    t_vec3d pos;
+	const t_basis3d	local = build_local_basis(cy.axis);
+	const t_vec3d	origin = sub_vec3d(cy.base.pos, nscale_vec3d(cy.axis, cy.h
+				* 0.5));
+	const t_ray		lray = local_ray(ray, local, origin);
+	t_vec3d			pos;
 
-    pos = get_point_on_ray(lray, t);
-    if (pos.y < 0 || pos.y > cy.h)
-        return false;
-    pos = point_from_basis(pos, local, origin);
-    if (!set_hit(NULL, pos, ray, hit))
-        return false;
-    pos = get_point_on_ray(lray, t); 
-    minus_vec3d(&pos, new_vec3d(0, pos.y, 0));
-    hit->normal = point_from_basis(pos, local, new_vec3d(0, 0, 0));
-    return true;
+	pos = get_point_on_ray(lray, t);
+	if (pos.y < 0 || pos.y > cy.h)
+		return (false);
+	pos = point_from_basis(pos, local, origin);
+	if (!set_hit(NULL, pos, ray, hit))
+		return (false);
+	pos = get_point_on_ray(lray, t);
+	minus_vec3d(&pos, new_vec3d(0, pos.y, 0));
+	hit->normal = point_from_basis(pos, local, new_vec3d(0, 0, 0));
+	return (true);
 }
--- src/geometry/paraboloid.c
+++ src/geometry/paraboloid.c
@@ -1,119 +1,126 @@
 #include "minirt.h"
-float solve_paraboloid_hit(t_ray ray, t_paraboloid para, float *t0, float *t1);
-t_paraboloid new_paraboloid(t_vec3d pos, t_vec3d axis, float focal);
-bool valid_params(float h, t_ray ray, float t0, float t1);
-float get_valid_param(float h, t_ray ray, float t0, float t1);
-t_vec3d surface_normal(t_vec3d hit, t_ray ray);
 
-bool paraboloid_hit(t_paraboloid *para, t_ray ray, t_hit *hit)
+float			solve_paraboloid_hit(t_ray ray, t_paraboloid para, float *t0,
+					float *t1);
+t_paraboloid	new_paraboloid(t_vec3d pos, t_vec3d axis, float focal);
+bool			valid_params(float h, t_ray ray, float t0, float t1);
+float			get_valid_param(float h, t_ray ray, float t0, float t1);
+t_vec3d			surface_normal(t_vec3d hit, t_ray ray);
+
+bool	paraboloid_hit(t_paraboloid *para, t_ray ray, t_hit *hit)
 {
-    t_basis3d p = build_local_basis(para->axis); // builds new coords along para axis, para.pos is the origin in world space
-    t_ray r = local_ray(ray, p, para->base.pos); // ray in new coords
-    float t[2];
-	float disc;
-	t_vec3d n_hit_pos;
+	const t_basis3d	p = build_local_basis(para->axis);
+	const t_ray		r = local_ray(ray, p, para->base.pos);
+	float			t[2];
+	float			disc;
+	t_vec3d			n_hit_pos;
 
-	disc = solve_paraboloid_hit(r, new_paraboloid(new_vec3d(0, 0, 0), new_vec3d(0, 1, 0), para->focal), &t[0], &t[1]);
+	disc = solve_paraboloid_hit(r, new_paraboloid(new_vec3d(0, 0, 0),
+				new_vec3d(0, 1, 0), para->focal), &t[0], &t[1]);
 	if (disc < 0.0f || (t[0] < 0.0f && t[1] < 0.0f))
-		return false;
-    if (!valid_params(para->h, r, t[0], t[1]))
-        return false;
-    t[0] = get_valid_param(para->h, r, t[0], t[1]);
-    n_hit_pos = get_point_on_ray(r, t[0]); // hit in object coords
-    n_hit_pos = point_from_basis(n_hit_pos, p, para->base.pos); // convert from object to world point
-    if (!set_hit(para, n_hit_pos, ray, hit))
-        return false;
-    n_hit_pos = get_point_on_ray(r, t[0]);
-    hit->normal = surface_normal(n_hit_pos, r);
-    hit->normal = point_from_basis(hit->normal, p, para->base.pos);
-    return true;
+		return (false);
+	if (!valid_params(para->h, r, t[0], t[1]))
+		return (false);
+	t[0] = get_valid_param(para->h, r, t[0], t[1]);
+	n_hit_pos = get_point_on_ray(r, t[0]);
+	n_hit_pos = point_from_basis(n_hit_pos, p, para->base.pos);
+	if (!set_hit(para, n_hit_pos, ray, hit))
+		return (false);
+	n_hit_pos = get_point_on_ray(r, t[0]);
+	hit->normal = surface_normal(n_hit_pos, r);
+	hit->normal = point_from_basis(hit->normal, p, para->base.pos);
+	return (true);
 }
 
-float solve_paraboloid_hit(t_ray r, t_paraboloid para, float *t0, float *t1)
+float	solve_paraboloid_hit(t_ray r, t_paraboloid para, float *t0, float *t1)
 {
-    float a;
-    float b; 
-    float c;
-    float disc;
-    float sqrt_disc;
+	float	a;
+	float	b;
+	float	c;
+	float	disc;
+	float	sqrt_disc;
 
-    normlize_vec3d(&r.udir);
-    a = (r.udir.z * r.udir.z + r.udir.x * r.udir.x);
-    b = 2 * (r.origin.z * r.udir.z + r.origin.x * r.udir.x) - 4 * para.focal * r.udir.y;
-    c = (r.origin.z * r.origin.z + r.origin.x * r.origin.x) - 4 * para.focal * r.origin.y;
-    disc = (b * b) - (4.0f * a * c);
-    if (disc >= 0.0f)
-    {
-        sqrt_disc = sqrt(disc);
-        *t0 = (-b - sqrt_disc) / (2.0f * a);
-        *t1 = (-b + sqrt_disc) / (2.0f * a);
-    }
-    return (disc);
+	normlize_vec3d(&r.udir);
+	a = (r.udir.z * r.udir.z + r.udir.x * r.udir.x);
+	b = 2 * (r.origin.z * r.udir.z + r.origin.x * r.udir.x) - 4 * para.focal
+		* r.udir.y;
+	c = (r.origin.z * r.origin.z + r.origin.x * r.origin.x) - 4 * para.focal
+		* r.origin.y;
+	disc = (b * b) - (4.0f * a * c);
+	if (disc >= 0.0f)
+	{
+		sqrt_disc = sqrt(disc);
+		*t0 = (-b - sqrt_disc) / (2.0f * a);
+		*t1 = (-b + sqrt_disc) / (2.0f * a);
+	}
+	return (disc);
 }
 
-
-bool valid_params(float h, t_ray ray, float t0, float t1)
+bool	valid_params(float h, t_ray ray, float t0, float t1)
 {
-    t_vec3d pos;
-    t_vec3d pos2;
-    
-    pos2 = get_point_on_ray(ray, t1);
-    pos = get_point_on_ray(ray, t0);
-    if (pos.y > h && pos2.y > h)
-        return false;
-    if (t0 > 0.0f && t1 > 0.0f)
-    {
-        pos = get_point_on_ray(ray, fminf(t0, t1)); // near hit
-        pos2 = get_point_on_ray(ray, fmaxf(t0, t1)); // far hit
-        if (pos.y < h || pos2.y < h)
-            return true;
-        return false;
-    }
-    if (t0 > 0)
-    {
-        pos = get_point_on_ray(ray, t0);
-        if (pos.y > h)
-            return false;
-        return true;
-    }
-    pos = get_point_on_ray(ray, t1);
-    return pos.y < h;
+	t_vec3d	pos;
+	t_vec3d	pos2;
+
+	pos2 = get_point_on_ray(ray, t1);
+	pos = get_point_on_ray(ray, t0);
+	if (pos.y > h && pos2.y > h)
+		return (false);
+	if (t0 > 0.0f && t1 > 0.0f)
+	{
+		pos = get_point_on_ray(ray, fminf(t0, t1));
+		pos2 = get_point_on_ray(ray, fmaxf(t0, t1));
+		if (pos.y < h || pos2.y < h)
+			return (true);
+		return (false);
+	}
+	if (t0 > 0)
+	{
+		pos = get_point_on_ray(ray, t0);
+		if (pos.y > h)
+			return (false);
+		return (true);
+	}
+	pos = get_point_on_ray(ray, t1);
+	return (pos.y < h);
 }
 
-float get_valid_param(float h, t_ray ray, float t0, float t1)
+float	get_valid_param(float h, t_ray ray, float t0, float t1)
 {
-    t_vec3d pos = get_point_on_ray(ray, t0);
-    t_vec3d pos2 = get_point_on_ray(ray, t1);
-    float t;
-    float t2;
+	t_vec3d	pos;
+	t_vec3d	pos2;
+	float	t;
+	float	t2;
 
-    if (t0 > 0.0f && t1 > 0.0f)
-    {
-        t = fminf(t0, t1);
-        t2 = fmaxf(t0, t1);
-        pos = get_point_on_ray(ray, t);
-        pos2 = get_point_on_ray(ray, t2);
-        if (pos.y < h)
-            return t;
-        if (pos2.y < h)
-            return t2;
-    }
-    if (t0 > 0)
-        return t0;
-    return t1;
+	pos = get_point_on_ray(ray, t0);
+	pos2 = get_point_on_ray(ray, t1);
+	if (t0 > 0.0f && t1 > 0.0f)
+	{
+		t = fminf(t0, t1);
+		t2 = fmaxf(t0, t1);
+		pos = get_point_on_ray(ray, t);
+		pos2 = get_point_on_ray(ray, t2);
+		if (pos.y < h)
+			return (t);
+		if (pos2.y < h)
+			return (t2);
+	}
+	if (t0 > 0)
+		return (t0);
+	return (t1);
 }
 
 /*
-    Given the surface implicit definition of paraboloid x^2 + z^2 - y = 0, the gradient ▽F=(2x, -1, 2z) vector at any
-    gives us the normal of the surface n = ▽F / |▽F|
+	Given the surface implicit definition of paraboloid x^2 + z^2 - y = 0,
+		the gradient ▽F=(2x, -1, 2z) vector at any
+	gives us the normal of the surface n = ▽F / |▽F|
 */
-t_vec3d surface_normal(t_vec3d hit, t_ray ray)
+t_vec3d	surface_normal(t_vec3d hit, t_ray ray)
 {
-    t_vec3d n;
-    (void)ray;
-    n = new_vec3d(2 * hit.x, -1.0f, 2 * hit.z);
-    if (dot_vec3d(nscale_vec3d(ray.udir, -1), n) < 0)
-        scale_vec3d(&n, -1);
-    normlize_vec3d(&n);
-    return n;
+	t_vec3d	n;
+
+	n = new_vec3d(2 * hit.x, -1.0f, 2 * hit.z);
+	if (dot_vec3d(nscale_vec3d(ray.udir, -1), n) < 0)
+		scale_vec3d(&n, -1);
+	normlize_vec3d(&n);
+	return (n);
 }
