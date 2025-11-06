ffe2574270812480789db3de0fdd512518e8d807
Zakaria Farah <zakaria.farah55@outlook.com>
2025-11-06 23:14:00 +0200

    minor fix

--- src/parsing/autosave_utils.c
+++ src/parsing/autosave_utils.c
@@ -2,14 +2,14 @@
 
 void	save_sphere(t_sphere s, t_object b)
 {
-	printf("sp  %.1f,%.1f,%.1f  %.1f  %.0f,%.0f,%.0f  ", b.pos.x, b.pos.y,
+	printf("sp  %.2f,%.2f,%.2f  %.1f  %.0f,%.0f,%.0f  ", b.pos.x, b.pos.y,
 		b.pos.z, s.r + s.r, b.color.r, b.color.g, b.color.b);
 	save_additional_vals(b);
 }
 
 void	save_cylinder(t_cylinder c, t_object b)
 {
-	printf("cy  %.1f,%.1f,%.1f  %.1f,%.1f,%.1f  %.1f  %.1f  %.0f,%.0f,%.0f  ",
+	printf("cy  %.2f,%.2f,%.2f  %.2f,%.2f,%.2f  %.1f  %.1f  %.0f,%.0f,%.0f  ",
 		b.pos.x, b.pos.y, b.pos.z, b.axis->x, b.axis->y, b.axis->z, c.r, c.h,
 		b.color.r, b.color.g, b.color.b);
 	save_additional_vals(b);
@@ -18,7 +18,7 @@ void	save_cylinder(t_cylinder c, t_object b)
 void	save_plane(t_plane p, t_object b)
 {
 	(void)p;
-	printf("pl  %.1f,%.1f,%.1f  %.1f,%.1f,%.1f  %.0f,%.0f,%.0f  ", b.pos.x,
+	printf("pl  %.2f,%.2f,%.2f  %.2f,%.2f,%.2f  %.0f,%.0f,%.0f  ", b.pos.x,
 		b.pos.y, b.pos.z, b.axis->x, b.axis->y, b.axis->z, b.color.r, b.color.g,
 		b.color.b);
 	save_additional_vals(b);
@@ -26,7 +26,7 @@ void	save_plane(t_plane p, t_object b)
 
 void	save_paraboloid(t_paraboloid p, t_object b)
 {
-	printf("pa  %.1f,%.1f,%.1f  %.1f,%.1f,%.1f  %.1f  %.1f  %.0f,%.0f,%.0f  ",
+	printf("pa  %.2f,%.2f,%.2f  %.2f,%.2f,%.2f  %.1f  %.1f  %.0f,%.0f,%.0f  ",
 		b.pos.x, b.pos.y, b.pos.z, b.axis->x, b.axis->y, b.axis->z, p.focal,
 		p.h, b.color.r, b.color.g, b.color.b);
 	save_additional_vals(b);
@@ -34,7 +34,7 @@ void	save_paraboloid(t_paraboloid p, t_object b)
 
 void	save_cube(t_cube c, t_object b)
 {
-	printf("cu  %.1f,%.1f,%.1f  %.1f  %.1f  %.1f,%.1f,%.1f  %.0f,%.0f,%.0f  ",
+	printf("cu  %.2f,%.2f,%.2f  %.1f  %.1f  %.2f,%.2f,%.2f  %.0f,%.0f,%.0f  ",
 		b.pos.x, b.pos.y, b.pos.z, c.w, c.h, b.axis->x, b.axis->y, b.axis->z,
 		b.color.r, b.color.g, b.color.b);
 	save_additional_vals(b);
