2e0f0d1fdae7f6492a94e3f0ce1b9409e09ae2e0
Julius Pellinen <julius.pellinen@gmail.com>
2025-11-06 20:03:28 +0200

    norm

--- src/MLX/scale_objects_utils.c
+++ src/MLX/scale_objects_utils.c
@@ -1,38 +1,38 @@
 #include "minirt.h"
 
-void    scale_cube(t_cube *cube, float dx, float dy, float dt)
+void	scale_cube(t_cube *cube, float dx, float dy, float dt)
 {
-    cube->w += OBJ_S_SPEED * -dx * dt;
-    cube->h += OBJ_S_SPEED * dy * dt;
-    if (cube->h <= 0.05f)
-        cube->h = 0.05f;
-    if (cube->w <= 0.05f)
-        cube->w = 0.05f;	
+	cube->w += OBJ_S_SPEED * -dx * dt;
+	cube->h += OBJ_S_SPEED * dy * dt;
+	if (cube->h <= 0.05f)
+		cube->h = 0.05f;
+	if (cube->w <= 0.05f)
+		cube->w = 0.05f;
 }
 
-void scale_paraboloid(t_paraboloid *para, float dx, float dy, float dt)
+void	scale_paraboloid(t_paraboloid *para, float dx, float dy, float dt)
 {
-    para->focal += OBJ_S_SPEED * -dx * dt;
-    para->h += OBJ_S_SPEED * dy * dt;
-    if (para->focal <= 0.05f)
-       para->focal = 0.05f;
-    if (para->h <= 0.05f)
-       para->h = 0.05f;
+	para->focal += OBJ_S_SPEED * -dx * dt;
+	para->h += OBJ_S_SPEED * dy * dt;
+	if (para->focal <= 0.05f)
+		para->focal = 0.05f;
+	if (para->h <= 0.05f)
+		para->h = 0.05f;
 }
 
-void scale_cylinder(t_cylinder *cy, float dx, float dy, float dt)
+void	scale_cylinder(t_cylinder *cy, float dx, float dy, float dt)
 {
-    cy->r += OBJ_S_SPEED * -dx * dt;
-    cy->h += OBJ_S_SPEED * dy * dt;
-    if (cy->r <= 0.05f)
-        cy->r = 0.05f;
-    if (cy->h <= 0.05f)
-        cy->h = 0.05f;
+	cy->r += OBJ_S_SPEED * -dx * dt;
+	cy->h += OBJ_S_SPEED * dy * dt;
+	if (cy->r <= 0.05f)
+		cy->r = 0.05f;
+	if (cy->h <= 0.05f)
+		cy->h = 0.05f;
 }
 
-void scale_sphere(t_sphere *sphere, float dx, float dt)
+void	scale_sphere(t_sphere *sphere, float dx, float dt)
 {
-    sphere->r += OBJ_S_SPEED * -dx * dt;
-    if ((*(t_sphere *)sphere).r <= 0.05f)
-        (*(t_sphere *)sphere).r = 0.05f;
+	sphere->r += OBJ_S_SPEED * -dx * dt;
+	if (sphere->r <= 0.05f)
+		sphere->r = 0.05f;
 }
