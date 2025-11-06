4ee62a7e5192fee14a3f63bce19559da148cec66
Julius Pellinen <julius.pellinen@gmail.com>
2025-11-06 14:38:47 +0200

    error check for textures

--- src/textures/texture.c
+++ src/textures/texture.c
@@ -4,17 +4,18 @@ void	apply_texture(t_hit *hit)
 {
 	t_object	*base;
 
-    if (!hit->obj)
-        return;
-    base = get_base_object(hit->obj);
-    if (base->texture.index < 0)
-        return;
-    if (base->type == PLANE)
-        plane_uv(*((t_plane *)hit->obj), hit);
-    else if (base->type == SPHERE)
-        sphere_uv(*((t_sphere *)hit->obj), hit);
-    else if (base->type == CUBE)
-        cube_uv(*((t_cube *)hit->obj), hit);
+	if (!hit->obj)
+		return ;
+	base = get_base_object(hit->obj);
+	if (base->texture.index < 0
+		|| base->texture.index >= get_engine()->textures.images->count)
+		return ;
+	if (base->type == PLANE)
+		plane_uv(*((t_plane *)hit->obj), hit);
+	else if (base->type == SPHERE)
+		sphere_uv(*((t_sphere *)hit->obj), hit);
+	else if (base->type == CUBE)
+		cube_uv(*((t_cube *)hit->obj), hit);
 }
 
 t_color	get_texel(mlx_texture_t *text, float u, float v)
