e16713685b0349e8a608e62b65e807fe489ec2e0
jackie jan <zakaria.farah55@outlook.com>
2025-11-07 14:49:59 +0200

    added autosave for skybox

--- src/parsing/parse_skybox.c
+++ src/parsing/parse_skybox.c
@@ -4,10 +4,11 @@ void	init_skybox(char **split)
 {
     t_engine *engine = get_engine();
 
-    if (engine->skybox || !split[1])
+    if (engine->skybox.txt || !split[1])
         return;
 	printf("Initializing skybox: %s\n", split[1]);
-	engine->skybox = mlx_load_png(split[1]);
-	if (!engine->skybox)
+	engine->skybox.txt = mlx_load_png(split[1]);
+	if (!engine->skybox.txt)
 		runtime_error(ft_strjoin("failed to load ", split[1]));
+	engine->skybox.src = ft_strdup(split[1]);
 }
