7a89bcfb1829f125853ae1bd07d781dcddd3b129
Zakaria Farah <zakaria.farah55@outlook.com>
2025-11-07 15:01:09 +0200

    norm fix

--- src/textures/skybox.c
+++ src/textures/skybox.c
@@ -1,18 +1,18 @@
 #include "minirt.h"
 
-t_color skycolor(t_ray *ray, int y)
+t_color	skycolor(t_ray *ray, int y)
 {
-    const t_skybox *skybox = &get_engine()->skybox;
-    float theta;
-    float phi;
-    float u;
-    float v;
-    
-    if (!skybox)
-        return (int_to_color(color_gradient(get_engine(), y)));
-    theta = atan2(ray->udir.z, ray->udir.x);
-    phi = acos(ray->udir.y);
-    u = (theta + PI) / (2*PI);
-    v = phi / PI;
-    return get_texel(skybox->txt, u, v);
+	const t_skybox	*skybox = &get_engine()->skybox;
+	float			theta;
+	float			phi;
+	float			u;
+	float			v;
+
+	if (!skybox)
+		return (int_to_color(color_gradient(get_engine(), y)));
+	theta = atan2(ray->udir.z, ray->udir.x);
+	phi = acos(ray->udir.y);
+	u = (theta + PI) / (2 * PI);
+	v = phi / PI;
+	return (get_texel(skybox->txt, u, v));
 }
