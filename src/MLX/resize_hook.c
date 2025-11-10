f6b0059206dcababb9dbf405f8c0680e9edd151a
jackie jan <zakaria.farah55@outlook.com>
2025-11-10 13:01:36 +0200

    added resize hooks

---
+++ src/MLX/resize_hook.c
@@ -0,0 +1,21 @@
+#include "minirt.h"
+
+void	resize_screen(int window_width, int window_height, void *eng)
+{
+	t_engine	*engine;
+
+	engine = (t_engine *)eng;
+	engine->update = true;
+	engine->moving = true;
+	engine->window.width = window_width;
+	engine->window.height = window_height;
+	engine->window.aspect_ratio = (float)window_width
+		/ (float)window_height;
+	wait_for_threads();
+	update_viewport(&engine->viewport, engine->window);
+	setup_threads(engine, false);
+	mlx_resize_image(engine->image, engine->window.width, engine->window.height);
+	mlx_resize_image(engine->image_buffer, engine->window.width, engine->window.height);
+	engine->lcamera = engine->camera;
+	engine->update = false;
+}
