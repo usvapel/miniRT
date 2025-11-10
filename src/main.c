cf049ec8f422a0e974430c56095e84c0eb57a9c5
julius <julius.pellinen@gmail.com>
2025-11-10 16:04:36 +0200

    mlx checks

--- src/main.c
+++ src/main.c
@@ -26,13 +26,19 @@ void	cleanup_and_exit(void)
 void	initialize_mlx(t_engine *engine)
 {
 	engine->mlx = mlx_init(800, 600, "miniRT | fps: 0", false);
+	if (!engine->mlx)
+		cleanup_and_exit();
 	mlx_get_monitor_size(0, &engine->window.width, &engine->window.height);
+	if (engine->window.width <= 0 || engine->window.height <= 0)
+		cleanup_and_exit();
 	mlx_set_window_size(engine->mlx, engine->window.width, engine->window.height);
 	engine->window.aspect_ratio = (float)engine->window.width
 		/ (float)engine->window.height;
 	mlx_terminate(engine->mlx);
 	engine->mlx = mlx_init(engine->window.width, engine->window.height,
 			"miniRT | fps: 0", true);
+	if (!engine->mlx)
+		cleanup_and_exit();
 }
 
 int	main(int ac, char **av)
@@ -49,7 +55,10 @@ int	main(int ac, char **av)
 			engine->window.height);
 	engine->image_buffer = mlx_new_image(engine->mlx, engine->window.width,
 			engine->window.height);
-	mlx_image_to_window(engine->mlx, engine->image, 0, 0);
+	if (!engine->image || !engine->image_buffer)
+		cleanup_and_exit();
+	if (mlx_image_to_window(engine->mlx, engine->image, 0, 0) < 0)
+		cleanup_and_exit();
 	gettimeofday(&engine->frame.start, NULL);
 	mlx_resize_hook(engine->mlx, resize_screen, engine);
 	mlx_loop_hook(engine->mlx, key_hook, engine);
