441d442d61382b3cc7a34c5a4d344cc86004b7f6
Zakaria Farah <zakaria.farah55@outlook.com>
2025-11-11 14:31:38 +0200

    norm fixes

--- include/minirt.h
+++ include/minirt.h
@@ -6,72 +6,71 @@
 /*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 13:05:20 by jpelline          #+#    #+#             */
-/*   Updated: 2025/11/06 21:57:04 by zfarah           ###   ########.fr       */
+/*   Updated: 2025/11/11 14:20:23 by zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
-#define MINIRT_H
+# define MINIRT_H
 
-#include "MLX42.h"
-#include "mlx_hooks.h"
+# include "MLX42.h"
+# include "camera.h"
+# include "engine.h"
+# include "geometry.h"
+# include "libft.h"
+# include "mlx_hooks.h"
+# include "parsing.h"
+# include "primitives.h"
+# include "ray.h"
+# include "textures.h"
+# include "vector.h"
+# include "viewport.h"
+# include <errno.h>
+# include <float.h>
+# include <math.h>
+# include <pthread.h>
+# include <stdatomic.h>
+# include <stdio.h>
+# include <sys/time.h>
 
-#include "camera.h"
-#include "engine.h"
-#include "geometry.h"
-#include "libft.h"
-#include "parsing.h"
-#include "primitives.h"
-#include "ray.h"
-#include "textures.h"
-#include "vector.h"
-#include "viewport.h"
-
-#include <errno.h>
-#include <float.h>
-#include <math.h>
-#include <pthread.h>
-#include <stdatomic.h>
-#include <stdio.h>
-#include <sys/time.h>
-
-#define SHADOW_BIAS 0.2f
-#define SHININESS 64.0f
-#define X_AXIS 0
-#define Y_AXIS 1
-#define OBJ_SPEED 0.0003
-#define OBJ_S_SPEED 0.0005
-#define PI 3.14159265358979323846
-#define QUALITY_DELAY_SECONDS 2
+# define SHADOW_BIAS 0.2f
+# define SHININESS 64.0f
+# define X_AXIS 0
+# define Y_AXIS 1
+# define OBJ_SPEED 0.0003
+# define OBJ_S_SPEED 0.0005
+# define PI 3.14159265358979323846
+# define QUALITY_DELAY_SECONDS 2
 
 // phong
-#define A_CONSTANT 1.0f
-#define LINEAR_COEFFICIENT 0.09f
-#define QUADRATIC_COEFFICIENT 0.032f
+# define A_CONSTANT 1.0f
+# define LINEAR_COEFFICIENT 0.09f
+# define QUADRATIC_COEFFICIENT 0.032f
 
 // needs cleaning in the future
-void input_parsing(t_engine *engine, char **av);
-t_engine *get_engine(void);
-float clamp(float value, float min, float max);
-void cleanup_and_exit();
-void draw_scene(void *eng);
-void draw_to_buffer(t_threads *t, int x, int y, int color);
-bool timer(int prev_sec, int stop);
-int get_seconds(t_engine *engine);
+t_engine	*get_engine(void);
+float		clamp(float value, float min, float max);
+void		cleanup_and_exit(void);
+void		draw_scene(void *eng);
+void		draw_to_buffer(t_threads *t, int x, int y, int color);
+bool		timer(int prev_sec, int stop);
+int			get_seconds(t_engine *engine);
 
-void phong_model(t_engine *engine, t_hit *hit);
-int objects_intersection(t_engine *engine, t_ray *ray, t_hit *hit);
-bool obj_intersection(void *obj, t_ray ray, t_hit *hit);
+void		phong_model(t_engine *engine, t_hit *hit);
+int			objects_intersection(t_engine *engine, t_ray *ray, t_hit *hit);
+bool		obj_intersection(void *obj, t_ray ray, t_hit *hit);
 
 // phong
-t_vec3d reflect(t_vec3d direction, t_vec3d normal);
+t_vec3d		reflect(t_vec3d direction, t_vec3d normal);
 
-void get_specular(t_engine *engine, t_hit *hit, t_phong *p,
-                  t_generic_light light);
-void get_diffuse(t_phong *p);
+void		get_specular(t_engine *engine, t_hit *hit, t_phong *p,
+				t_generic_light light);
+void		get_diffuse(t_phong *p);
 
-t_object *get_base_object(void *obj);
+t_object	*get_base_object(void *obj);
 
-void get_screen_resolution(int32_t *width, int32_t *height);
-void cleanup_data(void);
+// clean ups
+void		cleanup_data(void);
+void		cleanup_and_exit(void);
+
 #endif // MINIRT_T
--- include/ray.h
+++ include/ray.h
@@ -1,65 +1,69 @@
 #ifndef RAY_H
-#define RAY_H
+# define RAY_H
 
-#include "minirtthreads.h"
-#include "primitives.h"
-#include <stdbool.h>
+# include "minirt_threads.h"
+# include "primitives.h"
+# include <stdbool.h>
 
-#define EPSILON 1e-4
-#define BOUNCES 4
+# define EPSILON 1e-4
+# define BOUNCES 4
 
-typedef struct s_material {
-  float reflect;
-  float refract;
-  bool ignore;
-} t_material;
+typedef struct s_material
+{
+	float		reflect;
+	float		refract;
+	bool		ignore;
+}				t_material;
 
-typedef struct s_ray {
-  t_vec3d origin;
-  t_vec3d udir;
-} t_ray;
+typedef struct s_ray
+{
+	t_vec3d		origin;
+	t_vec3d		udir;
+}				t_ray;
 
 typedef struct s_hit
 {
-	t_vec3d pos;
-	t_color color;
-	bool prev_hit;
-	t_vec3d normal;
-	int type;
-	float distance;
-	void *obj;
-	int face_axis;
-	int ignore_setting_face_axis;
-	t_material material;
-}	t_hit;
+	t_vec3d		pos;
+	t_color		color;
+	bool		prev_hit;
+	t_vec3d		normal;
+	int			type;
+	float		distance;
+	void		*obj;
+	int			face_axis;
+	int			ignore_setting_face_axis;
+	t_material	material;
+}				t_hit;
 
-typedef struct s_refract {
-  bool front_face;
-  t_vec3d normal;
-  float eta_ratio;
-  bool should_reflect;
-  t_vec3d R;
-  t_ray reflected;
-  t_color reflect_color;
-  float reflectance;
-  float indice;
-} t_refract;
+typedef struct s_refract
+{
+	bool		front_face;
+	t_vec3d		normal;
+	float		eta_ratio;
+	bool		should_reflect;
+	t_vec3d		r;
+	t_ray		reflected;
+	t_color		reflect_color;
+	float		reflectance;
+	float		indice;
+}				t_refract;
 
-void *raytracer(void *engine);
-t_color trace_ray(t_threads *t, t_ray ray, int depth);
-t_vec3d get_point_on_ray(t_ray ray, float t);
-t_ray get_ray(int x, int y);
-bool set_hit(void *obj, t_vec3d new_hit, t_ray ray, t_hit *hit);
-bool closest_hit(t_vec3d pos, t_vec3d hit1, t_vec3d hit2);
-float nearest_t(float t0, float t1);
-t_ray local_ray(t_ray ray, t_basis3d local, t_vec3d local_origin);
+void			*raytracer(void *engine);
+t_color			trace_ray(t_threads *t, t_ray ray, int depth);
+t_vec3d			get_point_on_ray(t_ray ray, float t);
+t_ray			get_ray(int x, int y);
+bool			set_hit(void *obj, t_vec3d new_hit, t_ray ray, t_hit *hit);
+bool			closest_hit(t_vec3d pos, t_vec3d hit1, t_vec3d hit2);
+float			nearest_t(float t0, float t1);
+t_ray			local_ray(t_ray ray, t_basis3d local, t_vec3d local_origin);
 
 // refraction
-t_color handle_refraction(t_threads *t, t_refract *rf, t_ray ray, t_hit *hit);
+t_color			handle_refraction(t_threads *t, t_refract *rf, t_ray ray,
+					t_hit *hit);
 // reflection
-t_color handle_reflection(t_threads *t, t_ray ray, t_hit *hit,
-                          float reflectance);
-t_ray create_reflected_ray(t_vec3d hit_pos, t_vec3d normal, t_vec3d direction,
-                           bool offset_forward);
+t_color			handle_reflection(t_threads *t, t_ray ray, t_hit *hit,
+					float reflectance);
+t_ray			create_reflected_ray(t_vec3d hit_pos, t_vec3d normal,
+					t_vec3d direction, bool offset_forward);
 
 #endif // RAY_H
