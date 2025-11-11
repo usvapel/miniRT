81acb17e1f5923b636c537ee037ab416e8cd0b56
Zakaria Farah <zfarah@c2r5p11.hive.fi>
2025-11-11 14:17:52 +0200

    Refactored the main

--- src/main.c
+++ src/main.c
@@ -1,46 +1,5 @@
 #include "minirt.h"
 
-t_engine	*get_engine(void)
-{
-	static t_engine	engine;
-
-	return (&engine);
-}
-
-t_object	*get_base_object(void *obj)
-{
-	return ((t_object *)obj);
-}
-
-void	cleanup_and_exit(void)
-{
-	t_engine	*engine;
-
-	engine = get_engine();
-	thread_cleanup();
-	cleanup_data();
-	mlx_terminate(engine->mlx);
-	exit(1);
-}
-
-void	initialize_mlx(t_engine *engine)
-{
-	engine->mlx = mlx_init(800, 600, "miniRT | fps: 0", false);
-	if (!engine->mlx)
-		cleanup_and_exit();
-	mlx_get_monitor_size(0, &engine->window.width, &engine->window.height);
-	if (engine->window.width <= 0 || engine->window.height <= 0)
-		cleanup_and_exit();
-	mlx_set_window_size(engine->mlx, engine->window.width, engine->window.height);
-	engine->window.aspect_ratio = (float)engine->window.width
-		/ (float)engine->window.height;
-	mlx_terminate(engine->mlx);
-	engine->mlx = mlx_init(engine->window.width, engine->window.height,
-			"miniRT | fps: 0", true);
-	if (!engine->mlx)
-		cleanup_and_exit();
-}
-
 int	main(int ac, char **av)
 {
 	t_engine	*engine;
@@ -51,15 +10,6 @@ int	main(int ac, char **av)
 	input_parsing(engine, av);
 	initialize_mlx(engine);
 	setup_viewport();
-	engine->image = mlx_new_image(engine->mlx, engine->window.width,
-			engine->window.height);
-	engine->image_buffer = mlx_new_image(engine->mlx, engine->window.width,
-			engine->window.height);
-	if (!engine->image || !engine->image_buffer)
-		cleanup_and_exit();
-	if (mlx_image_to_window(engine->mlx, engine->image, 0, 0) < 0)
-		cleanup_and_exit();
-	gettimeofday(&engine->frame.start, NULL);
 	mlx_resize_hook(engine->mlx, resize_screen, engine);
 	mlx_loop_hook(engine->mlx, key_hook, engine);
 	mlx_loop_hook(engine->mlx, draw_scene, engine);
