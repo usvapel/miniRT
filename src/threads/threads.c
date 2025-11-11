b055385ba513876d14e365360b362d9c7a209419
Julius Pellinen <jpelline@c2r5p9.hive.fi>
2025-11-11 15:41:04 +0200

    fixes

--- src/threads/threads.c
+++ src/threads/threads.c
@@ -20,14 +20,20 @@ void	thread_cleanup(void)
 	}
 }
 
-static void	init_values(t_engine *engine, int i, int x_step, int y_step)
+static void	init_values(t_engine *engine, int i, bool create_threads)
 {
+	int	y_step;
+	int	x_step;
+
+	y_step = engine->window.height / THREAD_COUNT;
+	x_step = engine->window.width;
 	engine->threads[i].index = i;
 	engine->threads[i].start_y = y_step * i;
 	engine->threads[i].end_y = y_step * (i + 1);
 	engine->threads[i].start_x = 0;
 	engine->threads[i].end_x = x_step;
-	engine->threads[i].done = false;
+	if (create_threads)
+		engine->threads[i].done = false;
 	engine->threads[i].end = false;
 	engine->threads[i].block_size = PIXEL_BLOCK_SIZE;
 	engine->threads[i].last_move = 0;
@@ -36,17 +42,13 @@ static void	init_values(t_engine *engine, int i, int x_step, int y_step)
 void	setup_threads(void *eng, bool create_threads)
 {
 	t_engine	*engine;
-	int			y_step;
-	int			x_step;
 	int			i;
 
 	engine = eng;
-	y_step = engine->window.height / THREAD_COUNT;
-	x_step = engine->window.width;
 	i = 0;
 	while (i < THREAD_COUNT)
 	{
-		init_values(engine, i, x_step, y_step);
+		init_values(engine, i, create_threads);
 		if (create_threads && pthread_create(&engine->threads[i].thread, NULL,
 				raytracer, &engine->threads[i]))
 		{
